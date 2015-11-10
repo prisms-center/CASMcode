c59c9a0f891785091032bef1f154abc0a15abe56
bpuchala <bpuchala@users.noreply.github.com>
2015-11-10 02:14:55 -0500

    initial ideas for a python api for using CASM projects

---
+++ python/casm/casm/project/__init__.py
@@ -0,0 +1,4 @@
+"""An interface to CASM projects via Python"""
+from project import *
+from selection import *
+__all__ = dir()
---
+++ python/casm/casm/project/selection.py
@@ -0,0 +1,91 @@
+import project
+import os, subprocess
+
+class Selection(object):
+    """
+    A Selection object contains information about a CASM project
+    
+    Attributes:
+      proj: the CASM Project for this selection
+      path: absolute path to selection file, or "MASTER" for master list
+    
+    """
+    def __init__(self, proj=None, path="MASTER"):
+        """
+        Construct a CASM Project representation.
+
+        Args:
+            proj: a Project
+            path: path to selection file (Default="MASTER"). 
+            auto: True if using pbs module's JobDB to manage pbs jobs
+
+        """
+        if proj == None:
+          proj = project.Project()
+        elif not isinstance(proj, project.Project):
+          raise Exception("Error constructing Selection: proj argument is not a CASM project")
+        self.proj = proj
+        
+        if path == "MASTER":
+          self.path = "MASTER"
+        elif os.path.isfile(path):
+          self.path = os.path.abspath(path)
+        else:
+          raise Exception("No file exists named: " + path)
+        
+    
+    
+    def set_on(self, criteria="", output=None):
+        """
+        Perform 'casm select --set-on' using this Selection as input
+        
+        Args:
+            criteria: selection criteria string (Default="").
+            output: Name of output file to write result to
+        
+        """
+        self._generic_set("set-on", criteria, output)
+    
+    
+    def set_off(self, criteria="", output=None):
+        """
+        Perform 'casm select --set-off' using this Selection as input
+        
+        Args:
+            criteria: selection criteria string (Default="").
+            output: Name of output file to write result to
+        
+        """
+        self._generic_set("set-off", criteria, output)
+    
+    
+    def set(self, criteria="", output=None):
+        """
+        Perform 'casm select --set' using this Selection as input
+        
+        Args:
+            criteria: selection criteria string (Default="").
+            output: Name of output file to write result to
+        
+        """
+        self._generic_set("set", criteria, output)
+
+    
+    def _generic_set(self, command, criteria="", output=None):
+        """
+        Perform 'casm select --command' using this Selection as input, where
+        command='set-on','set-off', or 'set'
+        
+        Args:
+            criteria: selection criteria string (Default="").
+            output: Name of output file to write result to
+        
+        """
+        args = "casm select --" + command + " " + criteria + " -c " + self.path
+        if output != None:
+          args += " -o " + output
+        self.proj.command(args)
+        
+
+
+        
