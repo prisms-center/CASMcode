06069e2d3b5774ef876b241090cf440f17e019ea
bpuchala <bpuchala@users.noreply.github.com>
2015-12-23 15:36:43 -0600

    update 'casm view' help

--- apps/casm/view.cc
+++ apps/casm/view.cc
@@ -25,13 +25,12 @@ namespace CASM {
       
       ("configname",
         po::value<std::vector<std::string> >(&configname)->multitoken(),
-        "1 or more configname to view.")
+        "The name of 1 or more configurations to view. This is also a positional "
+        "argument, so '--configname' is optional.")
       
       ("config,c", 
         po::value<fs::path>(&selection), 
-        "Selected configurations to view.")
-        
-      ("force,f", "Overrwrite output file");
+        "Selected configurations to view.");
       
       
       // allow configname as positional options
@@ -48,10 +47,15 @@ namespace CASM {
           std::cout << "\n";
           std::cout << desc << std::endl;
 
-          std::cout << "This prepares input files for eci_search. In the specified         \n" <<
-                    "configuration list all selected configurations are used for printing\n" <<
-                    "an 'energy' file and 'corr.in' file that form the traning data for \n" <<
-                    "eci fitting. \n\n";
+          std::cout << "This allows opening visualization programs directly from \n" 
+                       "CASM. It iterates over all selected configurations and   \n"
+                       "one by one writes a POSCAR and executes                  \n"
+                       "   '$VIEW_COMMAND /path/to/POSCAR'                       \n"
+                       "where $VIEW_COMMAND is set via 'casm settings --set-view-command'.\n"
+                       "A script 'casm.view' is included with can be used to run \n"
+                       "a command and then pause 1s, which is useful for opening \n"
+                       "POSCARs with VESTA.  An example on Mac might look like:  \n"
+                       "  'casm settings --set-view-command 'casm.view \"open -a /Applications/VESTA/VESTA.app\"' \n\n";
 
           return 0;
         }
@@ -97,7 +101,6 @@ namespace CASM {
     PrimClex primclex(root, std::cout);
     std::cout << "  DONE." << std::endl << std::endl;
 
-    std::cout << "Reading config list" << std::endl;
     ConfigSelection<false> config_select;
     if(!vm.count("config")) {
       config_select = ConfigSelection<false>::None(primclex);
@@ -109,7 +112,6 @@ namespace CASM {
       config_select = ConfigSelection<false>(primclex, selection);
     }
     
-    std::cout << "Adding specific configurations" << std::endl;
     // add --configname (or positional) input
     for(int i=0; i<configname.size(); i++) {
       config_select.set_selected(configname[i], true);
@@ -118,7 +120,6 @@ namespace CASM {
     fs::path tmp_dir = root / ".casm" / "tmp";
     fs::create_directory(tmp_dir);
 
-    std::cout << "Loop over chosen configurations" << std::endl;
     // execute the 'casm view' command for each selected configuration
     for(auto it=config_select.selected_config_cbegin(); it!=config_select.selected_config_cend(); ++it) {
       // write '.casm/tmp/POSCAR'
