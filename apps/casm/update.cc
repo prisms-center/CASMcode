prisms-center/CASMcode

commit adb3407716fa9db0b8570f306a905d9af5c5d81e

implementing new chemical reference methods

apps/casm/update.cc
@@ -342,11 +342,6 @@ namespace CASM {
     else {
       std::cout << "Analyzed new data for " << num_updated << " configurations." << std::endl << std::endl;
 
-      std::cout << "Generating references... " << std::endl << std::endl;
-      /// This also re-writes the config_list.json
-      primclex.generate_references();
-      std::cout << "  DONE" << std::endl << std::endl;
-
       if(relax_log.str().size() > 0) {
         std::cout << "WARNING: Abnormal relaxations were detected:\n" << std::endl
                   << "           *** Final Relaxation Report ***" << std::endl

apps/casm/fit.cc
@@ -120,25 +120,6 @@ namespace CASM {
       config_select = ConfigSelection<false>(primclex, selection);
     }
 
-    // -- count number of selected configurations, and check they all have properties
-    bool ok = true;
-    int N_values = 0;
-    for(auto it = config_select.selected_config_cbegin(); it != config_select.selected_config_cend(); ++it) {
-      if(!it->delta_properties().contains("relaxed_energy")) {
-        std::cerr << "Configuration: " << it->name() << " does not have a formation energy." << std::endl;
-        ok = false;
-      }
-      N_values++;
-    }
-    if(!ok) {
-      std::cerr << "\nPlease check your configurations and re-try." << std::endl;
-      return 1;
-    }
-
-    if(N_values == 0) {
-      std::cerr << "\nDid not find any selected values. Please update your selection and re-try." << std::endl;
-    }
-
     // -- write 'energy' file ----
     {
       DataFormatter<Configuration> formatter(ConfigIO::formation_energy(),
@@ -161,7 +142,7 @@ namespace CASM {
       fs::ofstream sout;
       sout.open(corr_in_file);
       sout << N_corr << " # basis functions\n";
-      sout << N_values << " # training values\n";
+      sout << std::distance(config_select.selected_config_begin(), config_select.selected_config_end()) << " # training values\n";
       sout << "Correlation matrix:\n";
       sout << FormatFlag(sout).print_header(false);
       sout << formatter(config_select.selected_config_cbegin(), config_select.selected_config_cend());

apps/casm/status.cc
@@ -159,34 +159,35 @@ Instructions for volume relaxed VASP energies:                         \n\n\
     std::cout << "NEXT STEPS:\n\n";
 
     std::cout <<
-              "Set reference states\n\
-                                                                       \n\
-- References states can be set at the project, supercell or            \n\
-  configuration level. For a particular configuration (SCEL, CONFIGID),\n\
-  the following directories are checked for reference state files:     \n\
-  1) $ROOT/training_data/$SCELNAME/$CONFIGID/settings/$CURR_CALCTYPE/$CURR_REF\n\
-  2) $ROOT/training_data/$SCELNAME/settings/$CURR_CALCTYPE/$CURR_REF  \n\
-  3) $ROOT/training_data/settings/$CURR_CALCTYPE/$CURR_REF             \n\
-- Reference state files are named 'properties.ref_state.X.json', where \n\
-  X runs from 0 to the number of independent compositions              \n\
-- A reference property for a particular configuration is calculated    \n\
-  by linearly interpolating the value of the property in the reference \n\
-  states to the parametric composition of the configuration.           \n\
-                                                                       \n\
-Several options are possible:                                          \n\
-- Execute 'casm ref --set --auto' to automatically set project level   \n\
-  references using configurations with extreme parametric compositions.\n\
-                                                                       \n\
-- Execute 'casm ref --set --index REFID --s SCEL --c CONFIGID' to set a\n\
-  particular project level reference (REFID) to be the value of the    \n\
-  properties calculated for configuration (SCEL, CONFIGID).            \n\
-                                                                       \n\
-- Configuration or supercell level references can be set manually by   \n\
-  including 'properties.ref_state.X.json' files in the appropriate     \n\
-  directories (#1 and #2 above).                                       \n\
-                                                                       \n\
-- Execute 'casm ref --update' to update calculated reference properties\n\
-  if you have set reference state files manually.\n\n";
+              "Set chemical reference\n"
+"                                                                       \n"
+"- The chemical reference determines the value of the formation energy  \n"
+"  and chemical potentials calculated by CASM.                          \n"
+"- Chemical references states are set by specifying a hyperplane in     \n"
+"  energy/atom - composition (as atom_frac) space. This may be done by  \n"
+"  specifying the hyperplane explicitly, or by specifying several       \n"
+"  reference states with energy/atom and composition (as atom_frac) for \n"
+"  enough states to span the composition space of the allowed occupants \n"
+"  specified in the prim. For consistency with other CASM projects,     \n"
+"  additional reference states extending to other compositional         \n"
+"  dimensions may be included also.                                     \n"
+"- It is also possible to specialize the chemical reference at the      \n"
+"  supercell or configuration level.                                    \n"
+"                                                                       \n"
+"Several options are possible:                                          \n"
+"- Execute 'casm ref --set-auto' to automatically set project level     \n"
+"  references using configurations with extreme parametric compositions.\n"
+"                                                                       \n"
+"- Execute 'casm ref --set --index REFID --s SCEL --c CONFIGID' to set a\n"
+"  particular project level reference (REFID) to be the value of the    \n"
+"  properties calculated for configuration (SCEL, CONFIGID).            \n"
+"                                                                       \n"
+"- Configuration or supercell level references can be set manually by   \n"
+"  including 'properties.ref_state.X.json' files in the appropriate     \n"
+"  directories (#1 and #2 above).                                       \n"
+"                                                                       \n"
+"- Execute 'casm ref --update' to update calculated reference properties\n"
+"  if you have set reference state files manually.\n\n";
 
     std::cout <<
               "- See 'casm format' for a description and location of   \n\
@@ -551,55 +552,32 @@ Instructions for fitting ECI:                                          \n\n\
     std::cout << std::endl;
 
 
-    /// 5) Choose reference states
+    /// 5) Choose chemical reference
 
-    std::cout << "5) Choose reference states\n";
-    std::cout << "- Current reference set: " << primclex.get_curr_ref() << "\n";
-    std::cout << "- Configurations with / without references: " << std::flush;
-
-    int N_ref_set = 0;
-    int N_ref_unset = 0;
-    for(int i = 0; i < primclex.get_supercell_list().size(); i++) {
-      const Supercell &scel = primclex.get_supercell(i);
-      for(int j = 0; j < scel.get_config_list().size(); j++) {
-        fs::path dir = scel.get_config(j).get_reference_state_dir();
-        if(dir == fs::path()) {
-          N_ref_unset++;
-        }
-        else {
-          N_ref_set++;
-        }
-      }
+    std::cout << "5) Choose chemical reference\n";
+    std::cout << "- Chemical reference set: ";
+    if(primclex.has_chemical_reference()) {
+      std::cout << "TRUE" << "\n";
     }
+    else {
+      std::cout << "FALSE" << "\n";
+    }
+    std::cout << "\n";
+    
+    if(vm.count("details") ) {
+      ChemicalReferencePrinter p(std::cout, primclex.chemical_reference());
+      p.print_all();
+      std::cout << "\n\n";
+    }
+    else {
+      std::cout << "To show the chemical reference, run 'casm status -d'\n\n";
+    }
+    
+    
+    if(!primclex.has_chemical_reference()) {
 
-    std::cout << N_ref_set << " / " << N_ref_unset << std::endl << std::endl;
-
-    if(N_ref_unset > 0) {
-
-      if(vm.count("details")) {
-
-        if(N_ref_set == 0) {
-          std::cout << "No references are set." << std::endl << std::endl;
-        }
-        else {
-          std::cout << "Configurations missing references:" << std::endl;
-
-          for(int i = 0; i < primclex.get_supercell_list().size(); i++) {
-            const Supercell &scel = primclex.get_supercell(i);
-            for(int j = 0; j < scel.get_config_list().size(); j++) {
-              fs::path dir = scel.get_config(j).get_reference_state_dir();
-              if(dir == fs::path()) {
-                std::cout << std::setw(30) << scel.get_name() << "/" << j << std::endl;
-              }
-            }
-          }
-          std::cout << std::endl;
-        }
-      }
-      else {
-        std::cout << "For a list of configurations missing references, run 'casm status -d'\n\n";
-      }
-
+      std::cout << "No chemical reference chosen." << std::endl << std::endl;
+      
       if(vm.count("next")) {
         std::cout << "\n#################################\n\n";
 

include/casm/CASM_classes.hh
@@ -114,9 +114,17 @@
 #include "casm/clex/ConfigIOStrucScore.hh"
 #include "casm/clex/ConfigIOStrain.hh"
 #include "casm/clex/ConfigIOSelected.hh"
-
+#include "casm/clex/Norm.hh"
+#include "casm/clex/Reference.hh"
+#include "casm/clex/ChemicalReference.hh"
 
 // Hull
 #include "casm/hull/Hull.hh"
+
+// App
+#include "casm/app/DirectoryStructure.hh"
+#include "casm/app/ProjectSettings.hh"
+#include "casm/app/ProjectBuilder.hh"
+#include "casm/app/AppIO.hh"
 
 #endif

include/casm/app/AppIO.hh
@@ -4,8 +4,10 @@
 #include "casm/crystallography/BasicStructure.hh"
 #include "casm/symmetry/SymGroup.hh"
 #include "casm/clex/CompositionConverter.hh"
+#include "casm/clex/ChemicalReference.hh"
 
 #include "casm/casm_io/jsonParser.hh"
+#include "casm/casm_io/json_io/clex.hh"
 
 namespace CASM {
 
@@ -249,7 +251,86 @@ namespace CASM {
     json["multiplication_table"] = grp.get_multi_table();
     json["character_table"] = grp.get_character_table();
   }
+
   
+  // --------- ChemicalReference IO Declarations --------------------------------------------------
+  
+  ChemicalReference read_chemical_reference(fs::path filename, const Structure& prim, double tol = 1e-14);
+
+  ChemicalReference read_chemical_reference(const jsonParser& json, const Structure& prim, double tol = 1e-14);
+  
+  void write_chemical_reference(const ChemicalReference& chem_ref, fs::path filename);
+
+  void write_chemical_reference(const ChemicalReference& chem_ref, const jsonParser& json);
+  
+  
+  // --------- ChemicalReference IO Definitions --------------------------------------------------
+  
+  /// \brief Read chemical reference states from JSON file
+  ///
+  /// See documentation in related function for expected form of the JSON
+  inline ChemicalReference read_chemical_reference(fs::path filename,
+                                            const Structure& prim, 
+                                            double tol) {
+    try {
+      jsonParser json(filename);
+      return read_chemical_reference(json, prim, tol);
+    }
+    catch(...) {
+      std::cerr << "Error reading chemical reference states from " << filename << std::endl;
+      /// re-throw exceptions
+      throw;
+    }
+  }
+  
+  /// \brief Read chemical reference states from JSON
+  ///
+  /// Example expected form:
+  /// \code
+  /// { 
+  ///   "chemical_reference" : {
+  ///     "global" : ...,
+  ///     "supercell": {
+  ///       "SCELX": ...,
+  ///       "SCELY": ...
+  ///     },
+  ///     "config": {
+  ///       "SCELX/I": ...,
+  ///       "SCELY/J": ...
+  ///     }
+  ///   }
+  /// }
+  /// \endcode
+  ///
+  /// See one_chemical_reference_from_json for documentation of the \code {...} \endcode expected form.
+  inline ChemicalReference read_chemical_reference(const jsonParser& json,
+                                            const Structure& prim, 
+                                            double tol) {
+    
+    if(json.find("chemical_reference") == json.end()) {
+      throw std::runtime_error("Error reading chemical reference states: Expected \"chemical_reference\" entry");
+    }
+    
+    return jsonConstructor<ChemicalReference>::from_json(json["chemical_reference"], prim, tol);
+    
+  }
+  
+  inline void write_chemical_reference(const ChemicalReference& chem_ref, fs::path filename) {
+    SafeOfstream outfile;
+    outfile.open(filename);
+
+    jsonParser json;
+    write_chemical_reference(chem_ref, json);
+    json.print(outfile.ofstream());
+
+    outfile.close();
+  }
+
+  inline void write_chemical_reference(const ChemicalReference& chem_ref, jsonParser& json) {
+    json.put_obj();
+    to_json(chem_ref, json["chemical_reference"]);
+  }
+
 
   // --------- CompositionAxes Declarations --------------------------------------------------
 

include/casm/app/DirectoryStructure.hh
@@ -235,22 +235,12 @@ namespace CASM {
     fs::path composition_axes(std::string calctype, std::string ref) const {
       return ref_dir(calctype, ref) / "composition_axes.json";
     }
-
-    /// \brief Return reference state file path, for global settings
-    fs::path ref_state(std::string calctype, std::string ref, int index) const {
-      return ref_dir(calctype, ref) / _ref_state(index);
+    
+    /// \brief Return chemical reference file path
+    fs::path chemical_reference(std::string calctype, std::string ref) const {
+      return ref_dir(calctype, ref) / "chemical_reference.json";
     }
-
-    /// \brief Return reference state file path, for supercell specific settings
-    fs::path supercell_ref_state(std::string scelname, std::string calctype, std::string ref, int index) const {
-      return supercell_ref_dir(scelname, calctype, ref) / _ref_state(index);
-    }
-
-    /// \brief Return reference state file path, for configuration specific settings
-    fs::path configuration_ref_state(std::string configname, std::string calctype, std::string ref, int index) const {
-      return configuration_ref_dir(configname, calctype, ref) / _ref_state(index);
-    }
-
+    
 
     // -- Cluster expansions --------
 

include/casm/casm_io/json_io/clex.hh
@@ -1,0 +1,70 @@
+#ifndef CASM_jsonIO_clex
+#define CASM_jsonIO_clex
+
+#include "casm/casm_io/jsonParser.hh"
+#include "casm/CASM_global_definitions.hh"
+
+#include "casm/clex/ChemicalReference.hh"
+
+namespace CASM {
+  
+  class Structure;
+  
+  
+  // --- ChemicalReferenceState -------------------
+  
+  /// \brief Write ChemicalReferenceState to: '{"A" : X, "B" : X, ..., "energy_per_species" : X }'
+  jsonParser &to_json(const ChemicalReferenceState &ref_state, jsonParser &json);
+  
+  /// \brief Read ChemicalReferenceState from: '{"A" : X, "B" : X, ..., "energy_per_species" : X }'
+  template<> 
+  ChemicalReferenceState from_json<ChemicalReferenceState>(const jsonParser &json);
+
+  /// \brief Read ChemicalReferenceState from: '{"A" : X, "B" : X, ..., "energy_per_species" : X }'
+  void from_json(ChemicalReferenceState &ref_state, const jsonParser &json);
+  
+  
+  // --- HyperPlaneReference -------------------
+  
+  jsonParser &to_json(const HyperPlaneReference &ref, jsonParser &json);
+  
+  template<>
+  struct jsonConstructor<HyperPlaneReference> {
+    
+    static HyperPlaneReference from_json(const jsonParser &json, 
+                                         HyperPlaneReference::InputFunction f);
+  };
+
+  void from_json(HyperPlaneReference &ref, 
+                 const jsonParser &json, 
+                 HyperPlaneReference::InputFunction f);
+  
+  
+  // --- ChemicalReference -------------------
+  
+  /// \brief Write chemical reference
+  jsonParser &to_json(const ChemicalReference &ref, jsonParser &json);
+  
+  /// \brief Read chemical reference from one of 3 alternative forms
+  std::pair<Eigen::VectorXd, std::vector<ChemicalReferenceState> >
+  one_chemical_reference_from_json(const Structure& prim, 
+                                   const jsonParser& json);
+  
+  /// \brief Read chemical reference from JSON
+  template<>
+  struct jsonConstructor<ChemicalReference> {
+    
+    static ChemicalReference from_json(const jsonParser& json, 
+                                       const Structure& prim, 
+                                       double tol = 1e-14);
+  };
+
+  /// \brief Read chemical reference from JSON
+  void from_json(ChemicalReference &ref, 
+                 const jsonParser &json,
+                 const Structure& prim, 
+                 double tol = 1e-14);
+  
+}
+
+#endif

commit c74965cedae275e65879b8b537932f4d8b242777

Fixing casm update bug introduced by adb3407716fa9db0b8570f306a905d9af5c5d81e

apps/casm/update.cc
@@ -355,6 +355,10 @@ namespace CASM {
       }
 
     }
+    if(num_updated > 0) {
+      std::cout << "Writing to configuration database..." << std::endl << std::endl;
+      primclex.write_config_list();
+    }    
     return 0;
   }
 }