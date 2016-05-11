d78c5a807320a6bf71f7b7a6d7052d51ab86602b
bpuchala <bpuchala@users.noreply.github.com>
2016-05-11 14:28:05 -0400

    update python install and versioning

--- python/casm/make_setup.py
+++ python/casm/make_setup.py
@@ -1,21 +1,22 @@
-import subprocess
+import sys
 
-# use git branch for version
-version = subprocess.Popen('git rev-parse --abbrev-ref HEAD'.split(), stdout=subprocess.PIPE).communicate()[0].rstrip()
+if len(sys.argv) != 3:
+  print "Error running casm/make_setup.py."
 
-if version[0] == 'v':
-  version = version[1:]
-
-# get remote url
-url = subprocess.Popen('git config --get remote.origin.url'.split(), stdout=subprocess.PIPE).communicate()[0].rstrip()
+version = sys.argv[1]
+url = sys.argv[2]
 
 setup_string = \
-"""from distutils.core import setup
+"""import sys
+if sys.version_info >= (2, 6):
+  from setuptools import setup
+else:
+  from distutils.core import setup
 import glob
 setup(name='casm',
       version='{0}',
       url='{1}',
-      description='An interface between CASM and DFT codes.',
+      description='CASM Python API and interface with DFT codes.',
       packages=['casm','casm.vaspwrapper', 'casm.learn', 'casm.project', 'casm.plotting'],
       scripts=glob.glob('scripts/*')
       )
--- python/casm/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-import glob
-setup(name='casm',
-      version='0.2.X_coord_tests',
-      url='https://github.com/jcthomas/CASMCode-dev',
-      description='An interface between CASM and DFT codes.',
-      packages=['casm','casm.vaspwrapper', 'casm.learn', 'casm.project', 'casm.plotting'],
-      scripts=glob.glob('scripts/*')
-      )
