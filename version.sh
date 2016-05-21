ece163828860a8049fc5f027399a48775362a044
John Goiri <jg.goiri@gmail.com>
2016-05-21 13:29:49 -0700

    Bypassed setup.py with Makefile.am

--- version.sh
+++ version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+RELEASE="0.0.0"
 VERDIR=./src/casm/version
 GITHASH=$(git rev-parse --short HEAD)
 CHANGES=$(git status --porcelain --untracked-files=no)
@@ -8,4 +9,8 @@ if [ ! -z "$CHANGES" ]; then
     CHANGES=".changes"
 fi
 
-sed -e "s|MY_VERSION|$GITHASH$CHANGES|g" < $VERDIR/version_template.cc > $VERDIR/version.cc
+echo -ne $RELEASE+$GITHASH$CHANGES
+#Exchange commented line if release
+#echo -ne $RELEASE
+
+#sed -e "s|MY_VERSION|$GITHASH$CHANGES|g" < $VERDIR/version_template.cc > $VERDIR/version.cc
