af6cc22f96c59ad923e55cdb22146480c6c0b924
John Goiri <jg.goiri@gmail.com>
2016-05-24 01:14:52 -0700

    Wrote out some basic stuff to play around with it

---
+++ apps/casm-complete/complete.cc
@@ -0,0 +1,155 @@
+#ifndef COMPLETE_CC
+#define COMPLETE_CC
+
+#include "complete.hh"
+
+namespace Completer {
+  std::string strip_argument(const std::string &raw_input) {
+    std::string stripped = raw_input;
+
+    if(stripped[0] == '-') {
+      stripped.erase(stripped.begin() + 1, stripped.end());
+    }
+
+    if(stripped[0] == '-') {
+      stripped.erase(stripped.begin() + 1, stripped.end());
+    }
+
+    return stripped;
+  }
+
+  Suboption::Suboption(const std::string &init_longname, std::string init_short, ARG_TYPE init_expected_types):
+    m_long(init_longname),
+    m_short(init_short),
+    expected_arg(init_expected_types) {
+  }
+
+  std::string Suboption::long_tag() const {
+    return m_long;
+  }
+
+  std::string Suboption::short_tag() const {
+    return m_short;
+  }
+
+  bool Suboption::matches(const std::string &test_tag) const {
+    if(test_tag.size() == 2) {
+      return (test_tag == m_short);
+    }
+
+    else {
+      return (test_tag == m_long);
+    }
+
+    throw std::runtime_error("The impossible has occurred. Stepped through an if/else block and hit neither");
+  }
+
+  ARG_TYPE Suboption::argument_type() const {
+    return expected_arg;
+  }
+
+  //***************************************************************************************************//
+
+  Option::Option(const std::string &init_tag, const std::vector<Suboption> &init_allowed_subopts):
+    m_tag(init_tag),
+    m_avail_suboptions(init_allowed_subopts) {
+  }
+
+  std::string Option::tag() const {
+    return m_tag;
+  }
+
+  /**
+   * Runs through all the stored Suboptions and creates a list of each --long
+   * tag they have. The list of strings is then returned.
+   */
+
+  std::vector<std::string> Option::probe_suboptions() const {
+    std::vector<std::string> suboption_tag_list;
+    for(auto it = m_avail_suboptions.begin(); it != m_avail_suboptions.end(); ++it) {
+      suboption_tag_list.push_back(it->long_tag());
+    }
+
+    return suboption_tag_list;
+  }
+
+  /**
+   * Runs through all the available Suboptions trying to match the provided suboption_tag.
+   * If the suboption_tag matches, then return the expected type of arguments of that suboption,
+   * if the provided tag doesn't match anything, then default to returning VOID, i.e.
+   * no completion should be done by bash.
+   */
+
+  ARG_TYPE Option::probe_argument_type(const std::string &suboption_tag) const {
+    for(auto it = m_avail_suboptions.begin(); it != m_avail_suboptions.end(); ++it) {
+      if(it->matches(suboption_tag)) {
+        return it->argument_type();
+      }
+    }
+
+    return ARG_TYPE::VOID;
+  }
+
+  bool Option::matches(const std::string &test_tag) const {
+    return (test_tag == m_tag);
+  }
+
+  //***************************************************************************************************//
+
+  Engine::Engine(const std::vector<Option> &init_options):
+    m_avail_options(init_options) {
+  }
+
+  /**
+   * Runs through all the available Options and generates a list of all their tags.
+   * This list is then returned.
+   */
+
+  std::vector<std::string> Engine::probe_options() const {
+    std::vector<std::string> option_list;
+    for(auto it = m_avail_options.begin(); it != m_avail_options.end(); ++it) {
+      option_list.push_back(it->tag());
+    }
+
+    return option_list;
+  }
+
+  /**
+   * Run through all the Options and try to match the provided option_tag. If one of the Options
+   * has that tag, then return all the Suboptions it has as a list of --long formatted strings.
+   * If the proved tag doesn't match any options, return an empty list.
+   */
+
+  std::vector<std::string> Engine::probe_suboptions(const std::string &option_tag) const {
+    for(auto it = m_avail_options.begin(); it != m_avail_options.end(); ++it) {
+      if(it->matches(option_tag)) {
+        return it->probe_suboptions();
+      }
+    }
+
+    return std::vector<std::string>();
+  }
+
+  /**
+   * First run through all the Options and see if you can find one that matches the provided
+   * option_tag. If one is found, then run through all the available Suboptions of said tag
+   * and see if the provided Suboption_tag can be matched. If it can be matched, return the
+   * expected argument type of that Suboption.
+   *
+   * If either tag can't be found the routine returns VOID, i.e. bash completion shouldn't make
+   * any suggestions.
+   */
+
+  ARG_TYPE Engine::probe_argument_type(const std::string &option_tag, const std::string &suboption_tag) const {
+    for(auto it = m_avail_options.begin(); it != m_avail_options.end(); ++it) {
+      if(it->matches(option_tag)) {
+        return it->probe_argument_type(suboption_tag);
+      }
+    }
+
+    return ARG_TYPE::VOID;
+  }
+
+}
+
+#endif
--- apps/casm-complete/complete.hh
+++ apps/casm-complete/complete.hh
@@ -1,28 +1,129 @@
+#ifndef COMPLETE_HH
+#define COMPLETE_HH
+
 #include <string>
 #include <vector>
 
 #include "casm/CASM_global_definitions.hh"
 
 namespace Completer {
+
+  ///Remove "--" or "-" from beginning of string if it exists, and return as new string
+  std::string strip_argument(const std::string &raw_input);
+
+  /**
+   * When the Engine class isn't returning strings corresponding to options
+   * or suboptions that bash should complete, then the user is in the
+   * process of writing out an argument. By printing out keywords, the
+   * bash completer script can know what kind of arguments are needed for the
+   * casm command:
+   *
+   * VOID:        Don't complete anything
+   * PATH:        Suggest path completions to a file or directory
+   * COMMAND:     Suggest executables within the environment PATH
+   * SCELNAME:    Run through the PrimClex and suggest the enumerated supercell names
+   * QUERY:       Suggest the available query options
+   * OPERATORS:   Suggest the available operators casm knows how to use (TODO: This one might be tricky to implement)
+   */
+
   enum class ARG_TYPE {VOID, PATH, COMMAND, SCELNAME, QUERY, OPERATOR};
 
-  class ArgumentBase {
+  //*****************************************************************************************************//
+
+  /**
+   * A typical casm command takes 3 types of parameters:
+   *  Option: Things like "super", "enum", "monte", etc
+   *  Suboption: Anything that starts with "--" or "-", such as "--max" or "-z"
+   *  Argument: The argument you pass to the suboption, such as an integer or filename
+   *
+   * The Suboption class consists of a string to identify the --long option, a character
+   * to identify the -s(hort) version, and the type of arguments that are expected.
+   */
+
+  class Suboption {
   public:
 
+    ///Explicit construction
+    Suboption(const std::string &init_longname, std::string init_short, ARG_TYPE init_expected_types = ARG_TYPE::VOID);
+
+    ///Return long name in string format
+    std::string long_tag() const;
+
+    ///Return short name as char
+    std::string short_tag() const;
+
+    ///See if a provided string matches either the --long or -s(hort) tags. Expects leading '-' characters.
+    bool matches(const std::string &test_tag) const;
+
+    ///Return the expected types of arguments that follow *this
+    ARG_TYPE argument_type() const;
+
   private:
+
+    ///--long identifier (includes leading "--")
+    const std::string m_long;
+
+    ///-s(hort) identifyer (includes leading '-')
+    std::string m_short;
+
+    ///Type of arguments expected
+    const ARG_TYPE expected_arg;
   };
 
-  class SuboptionBase {
-  public:
 
-  private:
-  };
+  //*****************************************************************************************************//
+
+
+  /**
+   * A typical casm command takes 3 types of arguments:
+   *  Option: Things like "super", "enum", "monte", etc
+   *  Suboption: Anything that starts with "--" or "-", such as "--max" or "-z"
+   *  Argument: The argument you pass to the suboption, such as an integer or filename
+   *
+   * The Option class has a string that identifies it (e.g. "enum"), as well as a list
+   * of Suboptions that it can take.
+   */
+
 
   class Option {
+    typedef std::size_t size_type;
+
   public:
 
+    ///Construct manually passing all values needed
+    Option(const std::string &init_tag, const std::vector<Suboption> &init_allowed_subopts);
+
+    ///Construct with program options (eventually preferred)
+    //Option(const std::string &init_tag, const CASM::po::option_description &init_premade_descs);
+
+    ///Return the identifying name of *this (e.g. "super", "monte", etc)
+    std::string tag() const;
+
+    ///Return what the suboptions (--long format) for *this are
+    std::vector<std::string> probe_suboptions() const;
+
+    ///For a particular --suboption, get what kind of arguments are expected. suboption_tag should be pre-stripped.
+    ARG_TYPE probe_argument_type(const std::string &suboption_tag) const;
+
+    ///Append a new suboption to the option
+    void push_back(const Suboption &new_suboption);
+
+    ///Check if the given string corresponds to the tag of *this
+    bool matches(const std::string &test_tag) const;
+
   private:
+
+    ///Name that identifies this casm option (e.g. "monte", "init", etc)
+    std::string m_tag;  //Make this const once the default constructors of Engine are gone?
+
+    ///List of all the available --suboptions this option has
+    std::vector<Suboption> m_avail_suboptions;
+
   };
+
+
+  //*****************************************************************************************************//
+
 
   /**
    * A typical casm command takes 3 types of arguments:
@@ -36,6 +137,7 @@ namespace Completer {
 
   class Engine {
     typedef std::size_t size_type;
+
   public:
 
     ///Default constructor so you can push back your own things
@@ -45,16 +147,16 @@ namespace Completer {
     Engine(const std::vector<Option> &init_options);
 
     ///Construct by passing boost program options (eventually preferred so that it can update itself)
-    Engine(const std::vector<CASM::po::options_description> &init_premade_descs);
+    //Engine(const std::vector<CASM::po::options_description> &init_premade_descs);
 
     ///Get a list of all available options
     std::vector<std::string> probe_options() const;
 
     ///For a particular option, get the available suboptions
-    std::vector<std::string> probe_suboptions(const std::string &option) const;
+    std::vector<std::string> probe_suboptions(const std::string &option_tag) const;
 
     ///For a particular option with suboption, get what kind of arguments are expected
-    ARG_TYPE probe_argument_type(const std::string &option, const std::string &suboption) const;
+    ARG_TYPE probe_argument_type(const std::string &option_tag, const std::string &suboption_tag) const;
 
     ///Return the arguments that should be bash completed
     std::vector<std::string> probe_arguments(ARG_TYPE argument_type) const;
@@ -67,6 +169,13 @@ namespace Completer {
 
   private:
 
+    ///List of all the available (presumably casm) options (e.g. "monte", "init", etc)
     std::vector<Option> m_avail_options;
+
+    ///Pointer to the project PrimClex, so the Engine can know the existing SCEL names
+    //const *CASM::PrimClex m_pclex;
+
   };
 }
+
+#endif
