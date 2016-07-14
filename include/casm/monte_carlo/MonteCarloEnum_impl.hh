2098e1a091d9fb241d630a80d9185ded9eed3490
bpuchala <bpuchala@users.noreply.github.com>
2016-07-14 18:41:58 -0400

    working on monte carlo enumeration saving primitive configurations; seems to be a problem with canonical supercells?

--- include/casm/monte_carlo/MonteCarloEnum_impl.hh
+++ include/casm/monte_carlo/MonteCarloEnum_impl.hh
@@ -3,6 +3,7 @@
 
 #include "casm/monte_carlo/MonteCarloEnum.hh"
 #include "casm/clex/ConfigIterator.hh"
+#include "casm/clex/ConfigMapping.hh"
 
 namespace CASM {
 
@@ -98,18 +99,60 @@ namespace CASM {
     }
   }
 
+  namespace {
+
+    /// \brief Until we write one to get primitive Configurations directly from
+    /// non-primitive configurations
+    ///
+    /// returns configname and is_new
+    ///
+    std::pair<std::string, bool> _import_primitive(PrimClex &primclex, Supercell &scel, Index config_index) {
+
+      BasicStructure<Site> nonprim = scel.superstructure(config_index);
+
+      BasicStructure<Site> prim;
+      nonprim.is_primitive(prim);
+
+      int map_opt = ConfigMapper::none;
+      double tol = TOL;
+      double vol_tol = 0.25;
+      double lattice_weight = 0.5;
+      ConfigMapper configmapper(primclex, lattice_weight, vol_tol, map_opt, tol);
+
+      std::string imported_name;
+      Eigen::Matrix3d cart_op;
+      std::vector<Index> best_assignment;
+      jsonParser fullrelax_data;
+      bool is_new = configmapper.import_structure_occupation(prim,
+                                                             imported_name,
+                                                             fullrelax_data,
+                                                             best_assignment,
+                                                             cart_op,
+                                                             true);
+      return std::make_pair(imported_name, is_new);
+    }
+  }
+
   /// \brief Save configurations in the hall of fame to the config list
   template<typename Base>
   void MonteCarloEnum<Base>::save_configs() const {
-    Index index;
+    std::string underline = "------------";
 
     Log &log = this->_log();
 
     log.write("Enumerated configurations to master config list");
     log << "configuration enumeration check: " << m_check_args << "\n";
     log << "configuration enumeration metric: " << m_metric_args << "\n";
-    log << std::setw(16) << "configname" << std::setw(16) << "score" << "\n";
-    log << std::setw(16) << std::string("-", 12) << std::setw(16) << std::string("-", 12) << "\n";
+    log << std::setw(24) << "configname"
+        << std::setw(16) << "primitive"
+        << std::setw(16) << "new"
+        << std::setw(16) << "score"
+        << "\n";
+    log << std::setw(24) << underline
+        << std::setw(16) << underline
+        << std::setw(16) << underline
+        << std::setw(16) << underline
+        << "\n";
 
     // get canonical equivalent supercell
     Index Nscel = this->primclex().get_supercell_list().size();
@@ -125,19 +168,43 @@ namespace CASM {
 
     Index config_index;
     Supercell::permute_const_iterator permute_it;
+    bool is_new, is_prim;
+    std::string configname;
 
     // transform hall of fame configurations so that they fill the canonical
     // equivalent supercell, and add
     for(const auto &val : enum_halloffame()) {
-
-      std::cout << "fill supercell" << std::endl;
       Configuration config = fill_supercell(scel, val.second);
 
-      std::cout << "add config" << std::endl;
-      scel.add_config(config, config_index, permute_it);
+      jsonParser json_src;
+      std::stringstream ss;
+      ss << std::setprecision(6) << val.first;
+      json_src["monte_carlo_enumeration"]["metric"] = m_metric_args;
+      json_src["monte_carlo_enumeration"]["score"] = ss.str();
 
-      std::cout << "print result" << std::endl;
-      log << std::setw(16) << scel.get_config(config_index).name() << std::setw(16) << val.first << "\n";
+      is_new = scel.add_config(config, config_index, permute_it);
+      scel.get_config(config_index).push_back_source(json_src);
+      is_prim = is_primitive(config);
+      configname = scel.get_config(config_index).name();
+
+      log << std::setw(24) << configname
+          << std::setw(16) << (is_prim ? "primitive" : "non-primitive")
+          << std::setw(16) << (is_new ? "new" : "existing")
+          << std::setw(16) << val.first << std::endl;
+
+      // if not primitive, generate and import the primitive configuration
+      if(!is_prim) {
+        auto res = _import_primitive(this->_primclex(), scel, config_index);
+        configname = res.first;
+        is_new = res.second;
+        std::cout << configname << " " << is_new << std::endl;
+        this->_primclex().configuration(configname).push_back_source(json_src);
+
+        log << std::setw(24) << configname
+            << std::setw(16) << "primitive"
+            << std::setw(16) << (is_new ? "new" : "existing")
+            << std::setw(16) << val.first << std::endl;
+      }
     }
     this->_primclex().write_config_list();
     log << std::endl;
