886025a29cef713f2da14e55a02df56467f3ab69
Elizabeth Decolvenaere <liz.decolvenaere@gmail.com>
2016-11-04 17:53:28 -0700

    explicitly listing imports, etc

--- python/casm/casm/questwrapper/__init__.py
+++ python/casm/casm/questwrapper/__init__.py
@@ -1,4 +1,11 @@
 """A wrapper for running seqquest through casm"""
-from questwrapper import *
-from relax import *
-__all__ = dir()
+from questwrapper import QuestWrapperError, read_settings, write_settings, \
+  quest_input_file_names
+from relax import Relax
+__all__ = [
+  'Relax',
+  'QuestWrapperError',
+  'read_settings',
+  'write_settings',
+  'quest_input_file_names'
+]
