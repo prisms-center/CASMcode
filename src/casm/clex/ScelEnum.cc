3f86fe71ba61e89c508452faebe555886e57f952
bpuchala <bpuchala@users.noreply.github.com>
2016-11-10 11:57:18 -0500

    ScelEnum: supercell enumerators

---
+++ src/casm/clex/ScelEnum.cc
@@ -0,0 +1,25 @@
+#include "casm/clex/ScelEnum_impl.hh"
+
+namespace CASM {
+
+  namespace {
+    typedef std::vector<std::string>::iterator str_vec_it;
+  }
+
+  template class ScelEnumByNameT<true>;
+  template class ScelEnumByNameT<false>;
+
+  template ScelEnumByNameT<true>::ScelEnumByNameT(PrimClex &, str_vec_it, str_vec_it);
+  template ScelEnumByNameT<false>::ScelEnumByNameT(PrimClex &, str_vec_it, str_vec_it);
+
+  template class ScelEnumByPropsT<true>;
+  template class ScelEnumByPropsT<false>;
+
+  template class CASM_TMP::traits<ScelEnumT<true> >;
+  template class CASM_TMP::traits<ScelEnumT<false> >;
+  template class EnumInterface<ScelEnumT<true> >;
+  template class EnumInterface<ScelEnumT<false> >;
+  template class ScelEnumT<true>;
+  template class ScelEnumT<false>;
+
+}
