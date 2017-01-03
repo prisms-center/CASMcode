701f7bdf11343c682d1f9e26df4aaa02972b89c8
bpuchala <bpuchala@users.noreply.github.com>
2017-01-03 17:10:04 -0500

    add functions to OrbitGeneration for more flexibility, to get 'to_canonical' and 'from_canonical'

--- include/casm/symmetry/OrbitGeneration.hh
+++ include/casm/symmetry/OrbitGeneration.hh
@@ -50,9 +50,14 @@ namespace CASM {
       elements(m_element_compare) {
     }
 
-    /// \brief Try inserting an element
+    /// \brief Try inserting an element, after generating the canonical form
     std::pair<typename OrbitGeneratorSet<OrbitType>::iterator, bool> insert(const Element &test) {
       return elements.insert(m_generate_canonical(test));
+    }
+
+    /// \brief Try inserting an element, assuming it is in canonical form
+    std::pair<typename OrbitGeneratorSet<OrbitType>::iterator, bool> insert_canonical(const Element &test) {
+      return elements.insert(test);
     }
 
     /// \brief Construct Orbit from all generating elements
@@ -126,7 +131,8 @@ namespace CASM {
       const SymGroup &_generating_group,
       const SymCompareType &_sym_compare) :
       generating_group(_generating_group),
-      sym_compare(_sym_compare) {}
+      sym_compare(_sym_compare),
+      m_to_canonical(nullptr) {}
 
     /// \brief Applies symmetry to return an equivalent Element in a canonical form
     Element operator()(const Element &e) const {
@@ -135,10 +141,60 @@ namespace CASM {
         auto test = sym_compare.prepare(copy_apply(op, e));
         if(sym_compare.compare(result, test)) {
           result = test;
+          m_to_canonical = &op;
         }
       }
       return result;
     }
+
+    /// \brief After using call operator, this can be checked
+    const SymOp &to_canonical() const {
+      return *m_to_canonical;
+    }
+
+    /// \brief After using call operator, this can be checked
+    const SymOp &from_canonical() const {
+      return to_canonical().inverse();
+    }
+
+  private:
+
+    mutable const SymOp *m_to_canonical;
+  };
+
+  /// \brief Functor to find to check if element is in canonical form
+  ///
+  /// - Uses generating SymGroup, SymCompareType::prepare, SymCompareType::compare
+  ///
+  /// \ingroup OrbitGeneration
+  ///
+  template<typename _OrbitType>
+  struct IsCanonical {
+
+    typedef _OrbitType OrbitType;
+    typedef typename OrbitType::Element Element;
+    typedef typename OrbitType::SymCompareType SymCompareType;
+
+    const SymGroup &generating_group;
+    const SymCompareType &sym_compare;
+
+    IsCanonical(
+      const SymGroup &_generating_group,
+      const SymCompareType &_sym_compare) :
+      generating_group(_generating_group),
+      sym_compare(_sym_compare) {}
+
+    /// \brief Applies symmetry to check if any Element is greater than e
+    bool operator()(const Element &e) const {
+      return std::none_of(
+               generating_group.begin(),
+               generating_group.end(),
+      [&](const SymOp & op) {
+        auto test = sym_compare.prepare(copy_apply(op, e));
+        return sym_compare.compare(e, test);
+      });
+    }
+
   };
 
 }
