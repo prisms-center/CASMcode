7a0267019793e4654715fac2cfea88a0bee2e8a3
Julija <jvinckeviciute@gmail.com>
2017-01-31 12:47:32 -0800

    Made DiffTransConfiguration.hh file.

---
+++ include/casm/kinetics/DiffTransConfiguration.hh
@@ -0,0 +1,15 @@
+#ifndef CASM_DiffTransConfiguration
+#define CASM_DiffTransConfiguration
+
+namespace CASM {
+    class DiffTransConfiguration {
+        public:
+            DiffTransConfiguration();
+
+        protected:
+
+        private:
+    };
+}
+
+#endif
