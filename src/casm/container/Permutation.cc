fd606bbaa631c3e5956dc8d43153c1041aab12f9
bpuchala <bpuchala@users.noreply.github.com>
2017-02-22 17:58:20 -0500

    convert Permutation to use std::vector

--- src/casm/container/Permutation.cc
+++ src/casm/container/Permutation.cc
@@ -9,10 +9,10 @@ namespace CASM {
     /// Permutation array. If m_perm_array[i]==j, then an entry at index 'j' before permutation
     /// goes to index 'i' after permutation. Put another way, m_perm_array IS the result of performing
     /// the permutation to the integer list {0,1,2,...,size()-1}
-    Array<Index> m_perm_array;
+    std::vector<Index> m_perm_array;
 
   public:
-    Permutation(const Array<Index> &init_perm): m_perm_array(init_perm){};
+    Permutation(const std::vector<Index> &init_perm): m_perm_array(init_perm){};
 
 
     Index size() const { return m_perm_array.size();};
@@ -33,13 +33,13 @@ namespace CASM {
     /// const access of m_perm_array for doing low-level permutation algebra
     const Index&  operator[](Index i) const { return m_perm_array[i];};
 
-    /// Generate permuted copy of type-T Array
+    /// Generate permuted copy of type-T std::vector
     template<typename T>
-    Array<T> permute(const Array<T> &before_array) const;
+    std::vector<T> permute(const std::vector<T> &before_array) const;
 
-    /// Generate inversely permuted copy of type-T Array
+    /// Generate inversely permuted copy of type-T std::vector
     template<typename T>
-    Array<T> ipermute(const Array<T> &before_array) const;
+    std::vector<T> ipermute(const std::vector<T> &before_array) const;
 
 
   };
@@ -74,7 +74,10 @@ namespace CASM {
   void Permutation::append_fixed_points(Index N_new) {
     if(!N_new)
       return;
-    m_perm_array.append(Array<Index>::sequence(m_perm_array.size(), m_perm_array.size() + N_new - 1));
+    Index orig_size = m_perm_array.size();
+    for(Index i = 0; i < N_new; ++i) {
+      m_perm_array.push_back(i + orig_size);
+    }
   }
 
   //**************************************************************
@@ -82,29 +85,31 @@ namespace CASM {
   /// Construct permutation that undoes the permutation performed by 'this'
   /// Inverse operation is calculated the same, regardless of permutation convention
   Permutation Permutation::inverse() const {
-    Array<Index> im_perm_array(size(), 0);
+    std::vector<Index> im_perm_array(size(), 0);
     for(Index i = 0; i < size(); i++) {
       im_perm_array[m_perm_array[i]] = i;
     }
-    return Permutation(ReturnArray<Index>(im_perm_array));
+    return Permutation(std::move(im_perm_array));
   }
 
   //**************************************************************
   /// Construct permutation of dimension size()*block_dims.sum() that describes the effect of permuting
   /// N blocks, where the i'th block has dimension block_dims[i] (N=size()==block_dims.size())
-  Permutation Permutation::make_block_permutation(const Array<Index> &block_dims)const {
+  Permutation Permutation::make_block_permutation(const std::vector<Index> &block_dims)const {
     assert(block_dims.size() == size());
-    Array<Index> block_perm;
+    std::vector<Index> block_perm;
     block_perm.reserve(block_dims.sum()*size());
-    Array<Index> i_start(block_dims.size(), 0);
+    std::vector<Index> i_start(block_dims.size(), 0);
     for(Index i = 0; i + 1 < block_dims.size(); i++) {
       i_start[i + 1] = i_start[i] + block_dims[i];
     }
 
     for(Index i = 0; i < size(); i++) {
-      block_perm.append(Array<Index>::sequence(i_start[m_perm_array[i]], i_start[m_perm_array[i]] + block_dims[m_perm_array[i]] - 1));
+      for(Index j = 0; j < block_dims[m_perm_array[i]]; ++j) {
+        block_perm.push_back(i_start[m_perm_array[i]] + j);
+      }
     }
-    return Permutation(ReturnArray<Index>(block_perm));
+    return Permutation(std::move(block_perm));
   }
 
   //**************************************************************
@@ -119,7 +124,7 @@ namespace CASM {
 
     // Equivalent to Permutation(trans_perm*(*this)*trans_perm.inverse());
     // There's probably a faster element-wise implementation, but it would be confusing
-    return Permutation(ReturnArray<Index>(trans_perm.permute(permute(trans_perm.inverse().m_perm_array))));
+    return Permutation(std::move(trans_perm.permute(permute(trans_perm.inverse().m_perm_array))));
   }
 
   //**************************************************************
@@ -130,7 +135,7 @@ namespace CASM {
   /// I don't think this depends on definition of permutation convention
   Permutation Permutation::operator*(const Permutation &RHS) const {
     //not sure if this is obvious.
-    return Permutation(ReturnArray<Index>(permute(RHS.m_perm_array)));
+    return Permutation(permute(RHS.m_perm_array));
   }
 
   //**************************************************************
