0743a55a6b2ec011fd46d5a6d5679140c2610364
harsha <sriharsha.naga@gmail.com>
2017-03-15 19:03:45 -0700

    clened up the neb.py in vasp portion. Didi some testing with new code.

--- python/vasp/vasp/neb.py
+++ python/vasp/vasp/neb.py
@@ -1,17 +1,21 @@
-import os, math, sys, shutil, gzip
-sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0) ##sets unbuffered output
-import vasp, io
+import os, math, sys, shutil
+#sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0) ##sets unbuffered output
+import vasp
+import io
 
 class Neb(object):
     """The class contains all the funtions for performing Nudged Elastic Band(NEB) Calcutations with VASP
         
        Also supports VTST tools to perform Climbing Image NEB."""
 
-    def __init(self, calcdir = None, settings = None):
+    ##write more intro
+
+    def __init__(self, calcdir = None, settings = None):
         """
            Construct a VASP NEB calculation object
         
            Reads the settings from a neb.json """
+        ## write more intro
            
         print "Constructing a VASP NEB  calculation object"
         sys.stdout.flush()
@@ -26,7 +30,7 @@ class Neb(object):
 
         # find existing .../calcdir/run.run_index directories, store paths in self.rundir list
         self.rundir = []
-        self.errdir = [] ## keeps track of the error directories of present run
+        self.errdir = [] ## keeps track of the error directories of present run only
         self.update_rundir() 
         self.update_errdir() ## redundent but keep it for the flow
         
@@ -36,14 +40,41 @@ class Neb(object):
         else:
             self.settings = settings
 
-        ##set defult values ## To be done     
+        ##set defult values ## To be done ## check them
+        if not "npar" in self.settings:
+            self.settings["npar"] = None
+        if not "kpar" in self.settings:
+            self.settings["kpar"] = None
+        if not "ncore" in self.settings:
+            self.settings["ncore"] = None
+        if not "vasp_cmd" in self.settings:
+            self.settings["vasp_cmd"] = None
+        if not "ncpus" in self.settings:
+            self.settings["ncpus"] = None
+        if not "run_limit" in self.settings:
+            self.settings["run_limit"] = 10
+        if not "nrg_convergence" in self.settings:
+            self.settings["nrg_convergence"] = None
+        if not "compress" in self.settings:
+            self.settings["compress"] = []
+        if not "err_types" in self.settings:
+            self.settings["err_types"] = ['SubSpaceMatrixError']
+
+        ## temporary for testing
+        if not "extra_input_files" in self.settings:
+            self.settings["extra_input_files"] = []
+        if not "initial" in self.settings:
+            self.settings["initial"] = None
+        if not "n_images" in self.settings:
+            self.settings["n_images"] = 3
+
         ##make sure all the defaults are set in self.settings
 
         print "VASP NEB object constructed\n"
         sys.stdout.flush()
 
 
-   def add_rundir(self):
+    def add_rundir(self):
         """Make a new run.i directory"""
         os.mkdir(os.path.join(self.calcdir, "run." + str(len(self.rundir))))
         self.update_rundir()
@@ -81,18 +112,23 @@ class Neb(object):
         """ mv all files and directories (besides initdir) into initdir """
         ### still has to handle the images
 
-        print "Moving files into initial run directory:", initdir
+        print "Moving files and directories into initial run directory:", initdir
         initdir = os.path.abspath(initdir)
         for p in os.listdir(self.calcdir):
             if (p in (io.VASP_INPUT_FILE_LIST + self.settings["extra_input_files"])) and (os.path.join(self.calcdir, p) != initdir):
                 os.rename(os.path.join(self.calcdir,p), os.path.join(initdir,p))
+        ## copying the folders with image poscars into initdir
+        for i in range(settings["n_images"]+2):
+            folder_name = str(i).zfill(max(2, len(str(settings["n_images"]))+1 ))
+            shutil.move(os.path.join(self.calcdir,folder_name),initdir)
+
         print ""
         sys.stdout.flush()
 
         # Keep a backup copy of the base INCAR
         shutil.copyfile(os.path.join(initdir,"INCAR"),os.path.join(self.calcdir,"INCAR.base"))
 
-        # If an initial incar is called for, copy it in and set the appropriate flag
+        # If an initial incar is called for, copy it in and set the appropriate flag ## is this required?
         if (self.settings["initial"] != None) and (os.path.isfile(os.path.join(self.calcdir,self.settings["initial"]))):
             new_values = io.Incar(os.path.join(self.calcdir,self.settings["initial"])).tags
             io.set_incar_tag(new_values, initdir)
@@ -153,11 +189,12 @@ class Neb(object):
                 self.add_rundir()
                 vasp.continue_job(self.rundir[-2], self.rundir[-1], self.settings)
                 shutil.copyfile(os.path.join(self.relaxdir,"INCAR.base"),os.path.join(self.rundir[-1],"INCAR")) ## should it be enforced??
+                #self.continue_neb_job(self.rundir[-2], self.rundir[-1], self.settings) ##makes sure neb files are properly copied ## to be written
 
             else: ## redundent
-                # probably hit walltime
                 self.add_rundir()
                 vasp.continue_job(self.rundir[-2], self.rundir[-1], self.settings)
+                #self.continue_neb_job(self.rundir[-2], self.rundir[-1], self.settings) ##makes sure neb files are properly copied ## to be written
 
             while True:
                 # run vasp
@@ -166,35 +203,11 @@ class Neb(object):
 
                 # if no errors, continue
                 if result is None or self.not_converging():
-                    # # Check for actions that should be taken after the initial run ## is it required
-                    # if len(self.rundir) == 1:
-                    #     if self.settings["fine_ngx"]:
-                    #         outcarfile = os.path.join(self.rundir[-1], "OUTCAR")
-                    #         if not os.path.isfile(outcarfile):
-                    #             # This is an error but I'm not sure what to do about it
-                    #             pass
-                    #         else:
-                    #             init_outcar = io.Outcar(outcarfile)
-                    #             if not init_outcar.complete:
-                    #                 # This is an error but I'm not sure what to do about it
-                    #                 pass
-                    #             elif (init_outcar.ngx is None or
-                    #                   init_outcar.ngy is None or
-                    #                   init_outcar.ngz is None):
-                    #                 # This is an error but I'm not sure what to do about it
-                    #                 pass
-                    #             else:
-                    #                 ng_tags = {
-                    #                     "ngx" : init_outcar.ngx*2,
-                    #                     "ngy" : init_outcar.ngy*2,
-                    #                     "ngz" : init_outcar.ngz*2}
-                    #                 print ng_tags
-                    #                 io.set_incar_tag(ng_tags, self.relaxdir, "INCAR.base")
                     break
 
                 # else, attempt to fix first error
                 self.add_errdir()
-                os.mkdir(self.rundir[-1]) ##why??
+                os.mkdir(self.rundir[-1])
                 # self.add_rundir()
                 err = result.itervalues().next()
 
@@ -206,30 +219,9 @@ class Neb(object):
                 print ""
                 sys.stdout.flush()
 
-                # if (self.settings["backup"] != None) and len(self.rundir) > 1: ##is it required?
-                #     print "Restoring from backups:"
-                #     for f in self.settings["backup"]:
-                #         if os.path.isfile(os.path.join(self.rundir[-2], f + "_BACKUP.gz")):
-                #             f_in = gzip.open(os.path.join(self.rundir[-2], f + "_BACKUP.gz", 'rb'))
-                #             f_out = open(os.path.join(self.rundir[-1], f, 'wb'))
-                #             f_out.write(f_in.read())
-                #             f_in.close()
-                #             f_out.close()
-                #             print f, " restored!"
-                #     sys.stdout.flush()
-
             (status, task) = self.status()
             print "\n++  status:", status, "  next task:", task
             sys.stdout.flush()
-
-        # if status == "complete":
-        #     if not os.path.isdir(self.finaldir):
-        #         # mv final results to relax.final
-        #         print "mv", os.path.basename(self.rundir[-1]), os.path.basename(self.finaldir)
-        #         sys.stdout.flush()
-        #         os.rename(self.rundir[-1], self.finaldir)
-        #         self.rundir.pop()
-        #         vasp.complete_job(self.finaldir, self.settings)
 
         return (status, task)
 
@@ -245,19 +237,19 @@ class Neb(object):
 
             The second value is the current task, where 
             "setup" means to start running calculations in run.0 in the current folder
-            "new_run" indicates another calculation job is required as present run is incomplete and start a next run.
+            "new_run" indicates another calculation job is required as present run is incomplete and need to start a next run.
         """
 
         # check status of relaxation runs
         self.update_rundir()
 
-        # check if all complete
-        if io.job_complete(self.rundir[-1]):
-            return ("complete",None)
-
         # if not yet started
         if len(self.rundir) == 0:
             return ("incomplete", "setup")
+
+        # check if all complete
+        if io.job_complete(self.rundir[-1]):
+            return ("complete",None)
 
         # elif not converging, return 'not_converging' error
         if self.not_converging():
@@ -266,25 +258,10 @@ class Neb(object):
         # else if the latest run is not complete, continue with a new run
         return ("incomplete", 'new_run')
 
-
-
-    """ Required funtions in the class
-    
-    --> **setup(self,calcdir,settings) #still has to set up the folders for the images 
-    --> **run(self): ## clean up some feature. fine_nxg and backup
-    ** - Important to do
-
-    other functions
-    --> status(self): ## checks the status of the job ## may be more status options
-        
+    """
     To Do:
-    1) Edit io/kpoints.py file for new INCAR options
-    2) Edit io/vaspio.py and make a write_neb_vasp_input(INPUT)
-    3) dig up all new errors possible for NEB calcs
-    4) start editing neb.py at casm/vaspwrapper/ 
-    5) make vasp.neb, vasp.neb_setup executables along with casm-calc support to run the calculation
-
-    Things to discuss
-      1) n_images vs intermediate_pos
-      2) position of atoms in pos or information on which atoms moved.
-      3) make it executable independently ina any config folder. so where should the pos be? and should report already present non neb calc??
+    1) write continue_neb_job() function
+    2) dig up all new errors possible for NEB calcs
+    3) start editing neb.py at casm/vaspwrapper/ 
+    4) make vasp.neb, vasp.neb_setup executables along with casm-calc support to run the calculation
+    """
