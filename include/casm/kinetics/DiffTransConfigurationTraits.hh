f069857773cc3587bbcf618a25e81f9a40cf260d
Sanjeev Kolli <skk74@cornell.edu>
2017-05-26 12:54:34 -0700

    begin DiffTransConfigDatabase

---
+++ include/casm/kinetics/DiffTransConfigurationTraits.hh
@@ -0,0 +1,22 @@
+#ifndef CASM_DiffTransConfigurationTraits
+#define CASM_DiffTransConfigurationTraits
+
+#include <string>
+#include <vector>
+#include "casm/kinetics/DiffTransConfiguration.hh"
+namespace CASM {
+  template<typename T> struct traits;
+
+  namespace Kinetics {
+    class DiffTransConfiguration;
+  }
+
+  template<>
+  struct traits<CASM::Kinetics: DiffTransConfiguration> {
+    static const std::string name;
+    static const std::string short_name;
+    static bool name_compare(std::string A, std::string B);
+  };
+}
+
+#endif
