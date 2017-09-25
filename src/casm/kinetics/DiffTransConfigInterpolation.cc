8f170d45a07275be9853006500cfa5b92f0a25c2
naga sri harsha <harsha@169-231-104-116.wireless.ucsb.edu>
2017-09-25 16:55:36 -0700

    changes to cpp files to accept config specific n_images

--- src/casm/kinetics/DiffTransConfigInterpolation.cc
+++ src/casm/kinetics/DiffTransConfigInterpolation.cc
@@ -76,8 +76,8 @@ namespace CASM {
       // selcection by names
       // Constrct a DB selection of DiffTransConfiguration from json and enumoption inputs
       DB::Selection<DiffTransConfiguration> sel(primclex);
-      int n_images = kwargs["n_images"].get<int>(); // set defaults with get_else
-      std::string calctype = kwargs["calctype"].get<std::string>();
+      // int n_images = kwargs["n_images"].get<int>(); // set defaults with get_else
+      // std::string calctype = kwargs["calctype"].get<std::string>();
       Index i;
       for (const auto &config : sel.selected()){
         // Create a interpolation object
@@ -85,6 +85,8 @@ namespace CASM {
         i = 0;
         for (const auto &img_config: enumerator){
           // file_path = $project_dir/training_data/diff_trans/$diff_trans_name/$scelname/$configid/$image_number/POSCAR
+          int n_images = kwargs[config.name()]["n_images"].get<int>(); // set defaults with get_else
+          std::string calctype = kwargs[config.name()]["calctype"].get<std::string>();
           auto file_path = primclex.dir().configuration_calc_dir(config.name(), calctype);
           file_path += "N_images_" + std::to_string(n_images) + "/0" + std::to_string(i) + "/POSCAR";
           fs::ofstream file(file_path);
