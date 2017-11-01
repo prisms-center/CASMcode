cdce1f0e093fc3c7b90addb1a11c23272ee2ba6f
bpuchala <bpuchala@users.noreply.github.com>
2017-11-01 11:02:48 -0400

    revert version numbering to use tags

--- version.sh
+++ version.sh
@@ -4,5 +4,5 @@ stringfile=build-aux/casm_version.txt
 echo "Updating version string in $stringfile"
 mkdir -p build-aux
 #Be sure to not include a newline!
-# git describe --abbrev=6 --dirty --always --tags > build-aux/casm_version.txt
-echo "`git symbolic-ref HEAD 2> /dev/null | cut -b 12-`-`git log --pretty=format:\"%h\" -1`" >  build-aux/casm_version.txt
+git describe --abbrev=6 --dirty --always --tags > build-aux/casm_version.txt
+# echo "`git symbolic-ref HEAD 2> /dev/null | cut -b 12-`-`git log --pretty=format:\"%h\" -1`" >  build-aux/casm_version.txt
