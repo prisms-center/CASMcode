30c18b8c3adac2d337c04aeedfe86d12f67640c1
bpuchala <bpuchala@users.noreply.github.com>
2017-11-16 14:56:29 -0500

    method to get typename

---
+++ include/casm/misc/TypeInfo.hh
@@ -0,0 +1,35 @@
+#include <type_traits>
+#include <typeinfo>
+#include <utility>
+#include <memory>
+#include <string>
+#include <cstdlib>
+#include <cxxabi.h>
+
+namespace CASM {
+
+  /// Get type name as string, via type_name<T>()
+  ///
+  /// - Thanks to: https://stackoverflow.com/a/20170989
+  template<typename T>
+  constexpr std::string type_name() {
+    typedef typename std::remove_reference<T>::type TR;
+    std::unique_ptr<char, void(*)(void *)> name(
+      abi::__cxa_demangle(typeid(TR).name(), nullptr, nullptr, nullptr),
+      std::free);
+    std::string res = name.get();
+    if(std::is_const<TR>::value) {
+      res += " const";
+    }
+    if(std::is_volatile<TR>::value) {
+      res += " volatile";
+    }
+    if(std::is_lvalue_reference<T>::value) {
+      res += "&";
+    }
+    else if(std::is_rvalue_reference<T>::value) {
+      res += "&&";
+    }
+    return res;
+  }
+}
