b1bbd6ce77641205f5141268d39e1e4e0b2691ae
bpuchala <bpuchala@users.noreply.github.com>
2017-12-06 00:20:18 -0500

    Add WithinScelSymCompare for TestSupercell; add standard FCC test supercell class

--- tests/unit/TestSupercell.cc
+++ tests/unit/TestSupercell.cc
@@ -5,12 +5,14 @@ namespace test {
   TestSupercell::TestSupercell(const PrimClex &primclex, const Eigen::Matrix3i &T) :
     scel(&primclex, make_supercell(primclex.prim().lattice(), T)),
     prim_sym_compare(primclex),
-    scel_sym_compare(scel) {}
+    scel_sym_compare(scel),
+    within_scel_sym_compare(scel) {}
 
   TestSupercell::TestSupercell(const PrimClex &primclex, const Lattice &lat) :
     scel(&primclex, lat),
     prim_sym_compare(primclex),
-    scel_sym_compare(scel) {}
+    scel_sym_compare(scel),
+    within_scel_sym_compare(scel) {}
 
   const SymGroup &TestSupercell::scel_fg() const {
     if(!m_scel_fg.size()) {
--- tests/unit/TestSupercell.hh
+++ tests/unit/TestSupercell.hh
@@ -17,9 +17,27 @@ namespace test {
     Supercell scel;
     PrimPeriodicSymCompare<IntegralCluster> prim_sym_compare;
     ScelPeriodicSymCompare<IntegralCluster> scel_sym_compare;
+    WithinScelSymCompare<IntegralCluster> within_scel_sym_compare;
     const SymGroup &scel_fg() const;
 
     mutable SymGroup m_scel_fg;
+  };
+
+  /// AxBxC standard FCC cell
+  struct TestStandardFCCSupercell : test::TestSupercell {
+    TestStandardFCCSupercell(const PrimClex &fcc_primclex, Index A, Index B, Index C) :
+      test::TestSupercell(fcc_primclex, lattice(fcc_primclex, A, B, C)) {}
+
+    static Lattice lattice(const PrimClex &primclex, Index A, Index B, Index C) {
+      Eigen::Vector3d a, b, c;
+      std::tie(a, b, c) = primclex.prim().lattice().vectors();
+      Eigen::Vector3d standard_a = c + b - a;
+      Eigen::Vector3d standard_b = a - b + c;
+      Eigen::Vector3d standard_c = a + b - c;
+
+      return Lattice((1.0 * A) * standard_a, (1.0 * B) * standard_b, (1.0 * C) * standard_c);
+    }
+
   };
 }
 
