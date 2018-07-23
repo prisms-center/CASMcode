c970791034c84d2dfd37a8a759eb8486d2cbcdb3
bpuchala <bpuchala@users.noreply.github.com>
2018-07-23 14:20:20 -0400

    working on travis tests

--- build_scripts/install-miniconda-osx.sh
+++ build_scripts/install-miniconda-osx.sh
@@ -7,7 +7,7 @@ CASM_CONDA_BUILD_DIR="${CASM_CONDA_BUILD_DIR:-/tmp}"
 
 curl -sSL https://repo.continuum.io/miniconda/Miniconda${CASM_PYTHON_VERSION:0:1}-latest-MacOSX-x86_64.sh -o $CASM_CONDA_BUILD_DIR/miniconda.sh \
   && mkdir -p $CASM_CONDA_DIR \
-  && bash $BUILD_DIR/miniconda.sh -bfp $CASM_CONDA_DIR \
+  && bash $CASM_CONDA_BUILD_DIR/miniconda.sh -bfp $CASM_CONDA_DIR \
   && PATH="$CASM_CONDA_CASM_CONDA_DIR/bin:$PATH" \
   && rm -rf $CASM_CONDA_BUILD_DIR/miniconda.sh \
   && conda install -y \
