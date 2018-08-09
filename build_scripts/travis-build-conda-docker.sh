ccd26ac25484b1332625481ce595f7ecfff069ce
bpuchala <bpuchala@users.noreply.github.com>
2018-08-08 21:01:50 -0400

    update travis build scripts

--- build_scripts/travis-build-conda-docker.sh
+++ build_scripts/travis-build-conda-docker.sh
@@ -17,27 +17,24 @@
 set -e
 BUILD_SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 export CASM_BUILD_DIR=$(dirname $BUILD_SCRIPTS_DIR)
-. $CASM_BUILD_DIR/build_scripts/install-functions.sh
-. $CASM_BUILD_DIR/build_scripts/build_versions.sh
-detect_os
-
-# these should pick up the correct info from the travis environment, otherwise set yourself
 export CASM_REPO_SLUG=${CASM_REPO_SLUG:-$TRAVIS_REPO_SLUG}
 export CASM_GIT_ID_USER=${CASM_GIT_ID_USER:-${CASM_REPO_SLUG%/*}}
 export CASM_BRANCH=${CASM_BRANCH:-$TRAVIS_BRANCH}
+
 if [ -n "$TRAVIS_TAG" ]; then
   CASM_DEFAULT_CONDA_LABEL="main"
 else
   CASM_DEFAULT_CONDA_LABEL="dev"
 fi
 
+### Nothing past here should use travis-ci variables
+
 . $CASM_BUILD_DIR/build_scripts/install-functions.sh
+. $CASM_BUILD_DIR/build_scripts/build_versions.sh
 detect_os
 
+# these should pick up the correct info from the travis environment, otherwise set yourself
 check_var "CASM_CONDA_LABEL" "Conda channel label (\"dev\" or \"main\")" "$CASM_DEFAULT_CONDA_LABEL"
-
-#     CASM_DOCKER_CONTAINER
-#     CASM_DOCKER_INIT (ex: "yum install curl git -y")
 
 # build and push conda packages
 bash $CASM_BUILD_DIR/build_conda_docker.sh \
