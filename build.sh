1712df7062f8cd33312a9a5bd7f5dd0f04f82fe7
bpuchala <bpuchala@users.noreply.github.com>
2018-09-27 09:31:01 -0400

    casm-cpp conda packages are missing files. During 'conda build' on linux, it seems the PREFIX and CONDA_PREFIX are different. This is attempting to fix the casm-cpp install location so that CASM files are included in the conda package.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ export CASM_BUILD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pw
 . $CASM_BUILD_DIR/build_scripts/install-functions.sh
 detect_os
 check_var "CONDA_PREFIX" "Must have the conda environment activated"
-export CASM_PREFIX=$CONDA_PREFIX
+check_var "CASM_PREFIX" "CASM install location" "$CONDA_PREFIX"
 
 ### end initialization ###
 
--- conda-recipes/casm-cpp/xcode/build.sh
+++ conda-recipes/casm-cpp/xcode/build.sh
@@ -5,6 +5,7 @@ export CASM_BUILD_DIR=$(pwd)
 export CASM_BASH_COMPLETION_DIR=$PREFIX/.bash_completion.d
 export CASM_WRITE_BC_SHORTCUT=$PREFIX/.bash_completion
 export CASM_PYTHON="python"
+export CASM_PREFIX=$PREFIX
 
 . $CASM_BUILD_DIR/build_scripts/install-functions.sh
 bash $CASM_BUILD_DIR/build_scripts/install-bash-completion.sh
