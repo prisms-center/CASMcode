d33387edcc6702804ded4e77502764af3cb2426b
Jan Kloppenburg <jank@numphys.org>
2019-01-24 13:55:01 +0100

    modified:   python/casm/casm/aimswrapper/relax.py

--- python/casm/casm/aimswrapper/relax.py
+++ python/casm/casm/aimswrapper/relax.py
@@ -170,10 +170,10 @@ class Relax(object):
         db = JobDB()
         print("Calculation directory: ", self.calcdir)
         sub_id = db.select_regex_id("rundir", self.calcdir)
-        print("JobID:" + sub_id)
-        sys.stdout.flush()
 
         if sub_id is not []:
+            print("JobID:" + sub_id)
+            sys.stdout.flush()
             for j in sub_id:
                 job = db.select_job(j)
                 if job["jobstatus"] != "?":
