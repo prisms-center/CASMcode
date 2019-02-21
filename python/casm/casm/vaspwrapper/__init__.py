dbc7686283f9432543553c83edcad2701b8cf424
bpuchala <bpuchala@users.noreply.github.com>
2019-02-21 12:07:52 -0500

    fix Python merge issues so 'casm' script runs

--- python/casm/casm/vaspwrapper/__init__.py
+++ python/casm/casm/vaspwrapper/__init__.py
@@ -2,14 +2,12 @@
 from casm.vaspwrapper.vaspwrapper import VaspWrapperError, read_settings, write_settings, \
     vasp_input_file_names
 from casm.vaspwrapper.converge import Converge
-from casm.vaspwrapper.neb import Neb
 from casm.vaspwrapper.relax import Relax
-from casm.vaspwrapper.vasp_calculator_base import VaspCalcuatorBase
+from casm.vaspwrapper.vasp_calculator_base import VaspCalculatorBase
 __all__ = [
     'Converge',
-    'Neb',
     'Relax',
-    'VaspCalcuatorBase',
+    'VaspCalculatorBase',
     'VaspWrapperError',
     'read_settings',
     'write_settings',
