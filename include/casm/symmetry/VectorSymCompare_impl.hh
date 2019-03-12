d1e27e0adbe58c8a3342f90307112240ce29f03e
John C. Thomas <johnct@umich.edu>
2019-03-12 15:14:01 -0400

    Adding missing files for constructing orbits of vectors and subspaces

---
+++ include/casm/symmetry/VectorSymCompare_impl.hh
@@ -0,0 +1,83 @@
+#ifndef CASM_VectorSymCompare_impl
+#define CASM_VectorSymCompare_impl
+
+#include "casm/symmetry/VectorSymCompare.hh"
+#include "casm/symmetry/SymMatrixXd.hh"
+#include "casm/misc/CASM_Eigen_math.hh"
+
+
+namespace CASM {
+
+  /// \brief Return tolerance
+  template<typename Base>
+  double EigenSymCompare<Base>::tol() const {
+    return m_tol;
+  }
+
+
+  /// \brief Orders 'prepared' elements in the same orbit
+  ///
+  /// - Returns 'true' to indicate A < B
+  /// - Equivalence is indicated by \code !compare(A,B) && !compare(B,A) \endcode
+  /// - Assumes elements are 'prepared' before being compared
+  /// Implementation:
+  /// - First compares by number of sites in cluster
+  /// - Then compare all displacements, from longest to shortest
+  template<typename Base>
+  bool EigenSymCompare<Base>::invariants_compare_impl(const Element &A, const Element &B) const {
+    if(A.cols() == B.cols()) {
+      return CASM::compare(A.norm(), B.norm(), tol());
+    }
+    return A.cols() < B.cols();
+  }
+
+  /// \brief Compares 'prepared' elements
+  ///
+  /// - Returns 'true' to indicate B < A
+  /// - Equivalence is indicated by \code !compare(A,B) && !compare(B,A) \endcode
+  /// - Assumes elements are 'prepared' before being compared
+  template<typename Base>
+  bool EigenSymCompare<Base>::compare_impl(const Element &A, const Element &B) const {
+    //std::cout << "Comparing \n   " << A.transpose() << "\n   " << B.transpose() << "\n\n";
+    bool result = colmajor_lex_compare(B, A, tol());
+    //std::cout << "RESULT: " << result << "\n\n";
+    return result;
+  }
+
+
+  /// \brief Returns transformation that takes 'obj' to its prepared (canonical) form
+  ///
+  /// - For now returns pointer to SymPermutation object that encodes permutation due to sorting elements
+  template<typename Base>
+  std::unique_ptr<SymOpRepresentation> EigenSymCompare<Base>::canonical_transform_impl(Element const &obj)const {
+    return std::unique_ptr<SymOpRepresentation>(new SymMatrixXd(obj.colPivHouseholderQr().solve(derived().representation_prepare_impl(obj))));
+  }
+
+
+  /// \brief Prepare an element for comparison
+  ///
+  /// - Attempts to find a sparse set of spanning vectors, and sort them so that subspace matrix is nearly upper triangular (if possible)
+  /// - Also enures that first nonzero element of each row (if there is one) is positive
+  template<typename Element, typename SymApply>
+  Element SubspaceSymCompare<Element, SymApply>::
+  representation_prepare_impl(Element obj) const {
+    Eigen::MatrixXd result = Eigen::MatrixXd(reduced_column_echelon(obj, this->tol()).householderQr().householderQ()).leftCols(obj.cols());
+    Index col = 0;
+    for(Index row = 0; row < result.rows(); ++row) {
+      Index i = 0;
+      for(i = col; i < result.cols(); ++i) {
+        if(!almost_zero(result(row, i), this->tol())) {
+          if(result(row, i) < 0)
+            result.col(i) *= -1;
+          ++col;
+          break;
+        }
+      }
+    }
+    return result;
+  }
+
+
+}
+
+#endif
