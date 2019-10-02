8072205cd1b1b12b202f0a3dc8a14432ab2ebfc5
John Goiri <jg.goiri@gmail.com>
2019-10-02 15:01:11 -0700

    Changing the symmetry adapter
    All crystallography in/out will be the same SymType, which holds the
    matrix, translation and time reversal.

--- include/casm/crystallography/SymmetryAdapter.hh
+++ include/casm/crystallography/SymmetryAdapter.hh
@@ -2,26 +2,44 @@
 #define SYMMETRYADAPTER_HH
 
 #include "casm/external/Eigen/Dense"
+#include <tuple>
 #include <vector>
 
+/* namespace */
+/* { */
+/*     SymOpMatrixType symop_to_matrix(const CASM::SymOp &op); */
+
+/*     template<typename SymGroupType> */
+/*     std::vector<SymOpMatrixType> symop_to_matrix(const SymGroupType &group) { */
+/*       std::vector<SymOpMatrixType> casted_group; */
+/*       casted_group.reserve(group.size()); */
+/*       for(const auto &op : group) { */
+/*         casted_group.push_back(symop_to_matrix(op)); */
+/*       } */
+
+/*       return casted_group; */
+/*     } */
+/* } */
+
 namespace CASM {
-  class SymOp;
-  class SymGroup;
   namespace Adapter {
     typedef Eigen::Matrix3d SymOpMatrixType;
+    typedef Eigen::Vector3d SymOpTranslationType;
+    typedef bool SymOpTimeReversalType;
+    typedef std::tuple<SymOpMatrixType, SymOpTranslationType, SymOpTimeReversalType> SymOpType;
 
-    SymOpMatrixType symop_to_matrix(const CASM::SymOp &op);
+    SymOpMatrixType &get_matrix(SymOpType &op);
+    const SymOpMatrixType &get_matrix(const SymOpType &op);
 
-    template<typename SymGroupType>
-    std::vector<SymOpMatrixType> symop_to_matrix(const SymGroupType &group) {
-      std::vector<SymOpMatrixType> casted_group;
-      casted_group.reserve(group.size());
-      for(const auto &op : group) {
-        casted_group.push_back(symop_to_matrix(op));
-      }
+    SymOpTranslationType &get_translation(SymOpType &op);
+    const SymOpTranslationType &get_translation(const SymOpType &op);
 
-      return casted_group;
-    }
+    SymOpTimeReversalType get_time_reversal(const SymOpType &op);
+
+    SymOpType construct_sym_op(const SymOpMatrixType &sym_matrix,
+                               const SymOpTranslationType &sym_translation,
+                               SymOpTimeReversalType &sym_time_reversal);
+
   } // namespace Adapter
 
 } // namespace CASM
--- src/casm/crystallography/SymmetryAdapter.cc
+++ src/casm/crystallography/SymmetryAdapter.cc
@@ -1,11 +1,36 @@
 #include "casm/crystallography/SymmetryAdapter.hh"
-#include "casm/symmetry/SymOp.hh"
-#include "casm/symmetry/SymGroup.hh"
+
+namespace {
+  /* SymOpMatrixType symop_to_matrix(const CASM::SymOp &op) { */
+  /*   return op.matrix(); */
+  /* } */
+} // namespace
 
 namespace CASM {
   namespace Adapter {
-    SymOpMatrixType symop_to_matrix(const CASM::SymOp &op) {
-      return op.matrix();
+    SymOpMatrixType &get_matrix(SymOpType &op) {
+      return std::get<0>(op);
     }
+    const SymOpMatrixType &get_matrix(const SymOpType &op) {
+      return std::get<0>(op);
+    }
+
+    SymOpTranslationType &get_translation(SymOpType &op) {
+      return std::get<1>(op);
+    }
+    const SymOpTranslationType &get_translation(const SymOpType &op) {
+      return std::get<1>(op);
+    }
+
+    SymOpTimeReversalType get_time_reversal(const SymOpType &op) {
+      return std::get<2>(op);
+    }
+
+    SymOpType construct_sym_op(const SymOpMatrixType &sym_matrix,
+                               const SymOpTranslationType &sym_translation,
+                               SymOpTimeReversalType &sym_time_reversal) {
+      return std::make_tuple(sym_matrix, sym_translation, sym_time_reversal);
+    }
+
   } // namespace Adapter
 } // namespace CASM
