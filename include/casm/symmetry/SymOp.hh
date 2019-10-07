7b4a5d0dfd28d4b8892e1707a88bd76e8c80ee26
John Goiri <jg.goiri@gmail.com>
2019-10-07 14:53:13 -0700

    Teeny documentation change.

--- include/casm/symmetry/SymOp.hh
+++ include/casm/symmetry/SymOp.hh
@@ -210,8 +210,11 @@ namespace CASM {
 
   };
 
+  /// Accessor to allow conversion to xtal::SymOpType. Returns Cartesian transformation matrix.
   const SymOp::matrix_type &get_matrix(const SymOp &op);
+  /// Accessor to allow conversion to xtal::SymOpType. Returns translation vector (tau).
   const SymOp::vector_type &get_translation(const SymOp &op);
+  /// Accessor to allow conversion to xtal::SymOpType. Returns whether the symmetry operation is time reversal active.
   bool get_time_reversal(const SymOp &op);
 
 
