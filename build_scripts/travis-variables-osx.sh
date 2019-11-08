3fba649dcbbb406a7f262d98414fcd2ab98869f2
bpuchala <bpuchala@users.noreply.github.com>
2019-11-08 13:23:37 -0500

    enable customizing CASM_CONFIGFLAGS

--- build_scripts/travis-variables-osx.sh
+++ build_scripts/travis-variables-osx.sh
@@ -6,11 +6,11 @@ check_var "CASM_ZLIB_PREFIX" "Specify where zlib is installed" "$CASM_PREFIX"
 check_var "CASM_BASH_COMPLETION" "Specify where bash-completion is installed" "/usr/local/etc/bash_completion"
 check_var "CASM_BASH_COMPLETION_DIR" "Specify where to install the casm bash-completion script" "$CASM_PREFIX/.bash_completion.d"
 
-CASM_CONFIGFLAGS="--prefix=$CASM_PREFIX "
-CASM_CONFIGFLAGS+="--with-zlib=$CASM_ZLIB_PREFIX "
-CASM_CONFIGFLAGS+="--with-boost=$CASM_BOOST_PREFIX "
-CASM_CONFIGFLAGS+="--with-bash-completion-dir=$CASM_BASH_COMPLETION_DIR "
-export CASM_CONFIGFLAGS
+CASM_DEFAULT_CONFIGFLAGS="--prefix=$CASM_PREFIX "
+CASM_DEFAULT_CONFIGFLAGS+="--with-zlib=$CASM_ZLIB_PREFIX "
+CASM_DEFAULT_CONFIGFLAGS+="--with-boost=$CASM_BOOST_PREFIX "
+CASM_DEFAULT_CONFIGFLAGS+="--with-bash-completion-dir=$CASM_BASH_COMPLETION_DIR "
+export CASM_CONFIGFLAGS=${CASM_CONFIGFLAGS:-$CASM_DEFAULT_CONFIGFLAGS}
 
 check_var "CASM_CXXFLAGS" "Compiler flags" "-O3 -Wall -fPIC --std=c++11 -DNDEBUG -fcolor-diagnostics -Wno-deprecated-register -Wno-ignored-attributes -Wno-deprecated-declarations"
 check_var "CASM_LDFLAGS" "Linker flags" "-Wl,-rpath,$CASM_BOOST_PREFIX/lib"
