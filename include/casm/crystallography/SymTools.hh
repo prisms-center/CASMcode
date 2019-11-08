424d6bac0235d6c5d6948de08162118e1ab2ec87
bpuchala <bpuchala@users.noreply.github.com>
2019-11-08 08:09:37 -0500

    fix include problems from merge

--- include/casm/crystallography/SymTools.hh
+++ include/casm/crystallography/SymTools.hh
@@ -1,11 +1,11 @@
 #ifndef XTALSYMTOOLS_HH
 #define XTALSYMTOOLS_HH
 
-#include <vector>;
+#include <vector>
 #include "casm/crystallography/Adapter.hh"
-#include "casm/crystallography/LatticeIsEquivalent.hh"
-#include "casm/CASM_global_definitions.hh"
 #include "casm/crystallography/Coordinate.hh"
+#include "casm/crystallography/LatticeIsEquivalent.hh"
+#include "casm/global/definitions.hh"
 
 namespace CASM {
   namespace xtal {
