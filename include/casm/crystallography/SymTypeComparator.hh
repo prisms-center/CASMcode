022b41774c9b3280d30ecf32aad39f1bba2c85a0
John Goiri <jg.goiri@gmail.com>
2020-04-18 20:24:57 -0700

    xtal::SymOp comparators are binary now, with UnaryCompare_f template
    Added function_traits to make UnaryCompare_f work easily

--- include/casm/crystallography/SymTypeComparator.hh
+++ include/casm/crystallography/SymTypeComparator.hh
@@ -9,65 +9,53 @@ namespace CASM {
     class Lattice;
 
     /**
-     * Unary predicate for finding SymOp in a container such as std::vector.
+     * Binary functor for checking if two SymOp are equal.
      * Compares the transformation matrix and translation to within the CASM
      * tolerance, and also checks for time reversal match.
-     *
-     * Members provided at construction must still exist externally when
-     * using this functor.
      */
 
-    struct SymOpCompare_f : public std::unary_function<SymOp, bool> {
-      explicit SymOpCompare_f(const SymOp &target_operation, double tolerance) : m_target_operation(target_operation), m_tolerance(tolerance) {
+    struct SymOpCompare_f {
+      explicit SymOpCompare_f(double tolerance) : m_tolerance(tolerance) {
       }
-      bool operator()(const SymOp &possible_match) const;
+      bool operator()(const SymOp &lhs, const SymOp& rhs) const;
 
     private:
-      const SymOp m_target_operation;
       double m_tolerance;
     };
 
     /**
-     * Unary predicate for finding SymOp in a container such as std::vector.
+     * Binary functor for checking if two SymOp are equal.
      * Compares the transformation matrix and translation to within the CASM
      * tolerance, and also checks for time reversal match.
      * A lattice is given at construction so that comparisons return true if
      * the operation is equivalent by lattice translations.
-     *
-     * Members provided at construction must still exist externally when
-     * using this functor.
      */
 
-    struct SymOpPeriodicCompare_f : public std::unary_function<SymOp, bool> {
-      explicit SymOpPeriodicCompare_f(const SymOp &target_operation, const Lattice &periodicity_lattice, double tolerance)
-        : m_target_operation(target_operation), m_periodicity_lattice(periodicity_lattice), m_tolerance(tolerance) {
+    struct SymOpPeriodicCompare_f {
+      explicit SymOpPeriodicCompare_f(const Lattice &periodicity_lattice, double tolerance)
+        : m_periodicity_lattice(periodicity_lattice), m_tolerance(tolerance) {
       }
-      bool operator()(const SymOp &possible_match) const;
+      bool operator()(const SymOp &lhs, const SymOp& rhs) const;
 
     private:
-      const SymOp m_target_operation;
       const Lattice m_periodicity_lattice;
       double m_tolerance;
     };
 
     /**
-     * Unary predicate for finding SymOp in a container by considering
+     * Binary functor for checking if two SymOp are equal.
      * only the transformation matrix. When using this comparator
      * both translation and time reversal are ignored. Returns true
      * if the transformation matrix matches.
-     *
-     * Members provided at construction must still exist externally when
-     * using this functor.
      */
 
-    struct SymOpMatrixCompare_f : public std::unary_function<SymOp, bool> {
-      explicit SymOpMatrixCompare_f(const SymOp &target_operation, double tolerance)
-        : m_target_operation(target_operation), m_tolerance(tolerance) {
+    struct SymOpMatrixCompare_f {
+      explicit SymOpMatrixCompare_f(double tolerance)
+        : m_tolerance(tolerance) {
       }
-      bool operator()(const SymOp &possible_match) const;
+      bool operator()(const SymOp &lhs, const SymOp& rhs) const;
 
     private:
-      const SymOp m_target_operation;
       double m_tolerance;
     };
 
--- include/casm/misc/type_traits.hh
+++ include/casm/misc/type_traits.hh
@@ -1,6 +1,9 @@
 #ifndef CASM_type_traits
 #define CASM_type_traits
 
+#include <utility>
+#include <type_traits>
+
 namespace notstd {
   /// \brief Alias for void, to help SFINAE work
   template<typename... Ts>
@@ -11,6 +14,41 @@ namespace notstd {
   /// \brief Alias for void, to help SFINAE work
   template<typename... Ts>
   using void_t = typename make_void<Ts...>::type;
+  
+/// \brief Deduces return type and argument types for non-generic
+/// labmdas and functors.
+template <typename T>
+struct function_traits : public function_traits<decltype(&T::operator())>
+{
+};
+
+template <typename ClassType, typename ReturnType, typename... Args>
+struct function_traits<ReturnType (ClassType::*)(Args...) const>
+{
+    enum
+    {
+        arity = sizeof...(Args)
+    };
+
+    typedef ReturnType result_type;
+
+    template <std::size_t i>
+    struct arg
+    {
+        typedef typename std::tuple_element<i, std::tuple<Args...>>::type type;
+    };
+};
+
+template <typename F>
+/* using first_argument_type = std::decay_t<typename function_traits<F>::template arg<0>::type>; */
+using first_argument_type = typename std::decay<typename function_traits<F>::template arg<0>::type>::type;
+template <typename F>
+/* using second_argument_type = std::decay_t<typename function_traits<F>::template arg<1>::type>; */
+using second_argument_type = typename std::decay<typename function_traits<F>::template arg<1>::type>::type;
+template <typename F>
+/* using return_type = std::decay_t<typename function_traits<F>::result_type>; */
+using return_type = typename std::decay<typename function_traits<F>::result_type>::type;
+
 }
 
 #endif
