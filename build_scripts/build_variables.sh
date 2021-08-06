e4aae1a184a2abd55cf977c2709a1feff5708f85
bpuchala <bpuchala@users.noreply.github.com>
2021-08-06 14:45:26 -0400

    allow configuration of build strings

--- build_scripts/build_variables.sh
+++ build_scripts/build_variables.sh
@@ -11,17 +11,18 @@ check_var "CASM_BRANCH" "Which branch to build"
 export CASM_REPO="https://github.com/"$CASM_GIT_ID_USER"/CASMcode.git"
 export CASM_URL=$CASM_REPO
 
-# osx xcode (use system compilers)
-export CASM_BOOST_XCODE_BUILD_NUMBER="0"
-export CASM_BOOST_XCODE_BUILD_STR="xcode_"$CASM_BOOST_XCODE_BUILD_NUMBER
-export CASM_XCODE_BUILD_NUMBER=0
-export CASM_XCODE_BUILD_STR="xcode_"$CASM_XCODE_BUILD_NUMBER
-
-# linux condagcc (use conda dist compilers)
-export CASM_BOOST_CONDAGCC_BUILD_NUMBER="1"
-export CASM_BOOST_CONDAGCC_BUILD_STR="condagcc${CASM_CONDAGCC_VERSION}_${CASM_BOOST_CONDAGCC_BUILD_NUMBER}"
-export CASM_CONDAGCC_BUILD_NUMBER=0
-export CASM_CONDAGCC_BUILD_STR="condagcc${CASM_CONDAGCC_VERSION}_${CASM_CONDAGCC_BUILD_NUMBER}"
+# set build number and build strings
+if [ "$CASM_OS_NAME" == "osx" ]; then
+  check_var "CASM_BOOST_XCODE_BUILD_NUMBER" "CASM osx boost build number"
+  export CASM_BOOST_XCODE_BUILD_STR="xcode_"$CASM_BOOST_XCODE_BUILD_NUMBER
+  check_var "CASM_XCODE_BUILD_NUMBER" "CASM osx build number"
+  export CASM_XCODE_BUILD_STR="xcode_"$CASM_XCODE_BUILD_NUMBER
+elif [ "$CASM_OS_NAME" == "linux" ]; then
+  check_var "CASM_BOOST_CONDAGCC_BUILD_NUMBER" "CASM linux boost build number"
+  export CASM_BOOST_CONDAGCC_BUILD_STR="condagcc${CASM_CONDAGCC_VERSION}_${CASM_BOOST_CONDAGCC_BUILD_NUMBER}"
+  check_var "CASM_CONDAGCC_BUILD_NUMBER" "CASM linux build number"
+  export CASM_CONDAGCC_BUILD_STR="condagcc${CASM_CONDAGCC_VERSION}_${CASM_CONDAGCC_BUILD_NUMBER}"
+fi
 
 # choose $(conda_version) or "X.Y.Z"
 check_var "CASM_CONDA_VERSION" "Version number for conda package"
