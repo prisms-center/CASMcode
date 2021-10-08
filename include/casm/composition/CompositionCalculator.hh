e72d833f53f1c8f1e5919652a2d29b30cfa4633e
bpuchala <bpuchala@users.noreply.github.com>
2021-10-08 10:46:29 -0400

    remove crystallography dependence from composition

--- include/casm/composition/CompositionCalculator.hh
+++ include/casm/composition/CompositionCalculator.hh
@@ -1,31 +1,29 @@
 #ifndef CASM_CompositionCalculator
 #define CASM_CompositionCalculator
 
+#include <set>
 #include <string>
 #include <vector>
 
-#include "casm/crystallography/LinearIndexConverter.hh"
+#include "casm/global/definitions.hh"
 #include "casm/global/eigen.hh"
 
 namespace CASM {
 
 /// \brief Calculate composition from occupation vectors
+///
+/// Note:
+/// - Assumes occupation vector is organized in sublattice blocks:
+///       [sublat 0 ---> | sublat 1 ---> | .... ]
+/// - Order of unit cells within a sublattice blocks does not matter.
 class CompositionCalculator {
  public:
   /// \brief Constructor
-  ///
-  /// \param unitcellcoord_index_converter Converts between site index (index
-  ///     into occupation vector) and UnitCellCoord
-  /// \param components Names of components corresponding to each position in
-  ///     the result. Must be consistent with occ_to_component_index_converter.
-  ///     Vacancy components are detected using `xtal::is_vacancy`.
-  /// \param occ_to_component_index_converter Lookup table for sublattice index
-  ///     and occupant index to component index. May be obtained from
-  ///     `xtal::make_index_converter`.
   CompositionCalculator(
-      xtal::UnitCellCoordIndexConverter const &_unitcellcoord_index_converter,
       std::vector<std::string> const &_components,
-      std::vector<std::vector<Index>> const &_occ_to_component_index_converter);
+      std::vector<std::vector<Index>> const &_occ_to_component_index_converter,
+      std::set<std::string> const &_vacancy_names =
+          std::set<std::string>({"Va", "VA", "va"}));
 
   /// \brief The order of components in composition vector results
   std::vector<std::string> components() const;
@@ -43,10 +41,6 @@ class CompositionCalculator {
   Eigen::VectorXd species_frac(Eigen::VectorXi const &occupation) const;
 
  private:
-  // Converts between site index (index into occupation vector) and
-  // UnitCellCoord
-  xtal::UnitCellCoordIndexConverter m_unitcellcoord_index_converter;
-
   // Names of components corresponding to each position in
   // the result. Must be consistent with occ_to_component_index_converter.
   // Vacancy components are detected using `xtal::is_vacancy`.
@@ -56,10 +50,6 @@ class CompositionCalculator {
   // and occupant index to component index. May be obtained from
   // `xtal::make_index_converter`.
   std::vector<std::vector<Index>> m_occ_to_component_index_converter;
-
-  // Integer volume (number of prim per supercell), cast as double to use for
-  // normalization
-  double m_volume;
 
   // Whether one of the components is a vacancy, as detected by
   // `xtal::is_vacancy`
--- src/casm/composition/CompositionCalculator.cc
+++ src/casm/composition/CompositionCalculator.cc
@@ -1,31 +1,31 @@
 #include "casm/composition/CompositionCalculator.hh"
 
-#include "casm/crystallography/Molecule.hh"
-
 namespace CASM {
 
 /// \brief Constructor
 ///
-/// \param unitcellcoord_index_converter Converts between site index (index
-///     into occupation vector) and UnitCellCoord
-/// \param components Names of components corresponding to each position in
+/// \param _components Names of components corresponding to each position in
 ///     the result. Must be consistent with occ_to_component_index_converter.
 ///     Vacancy components are detected using `xtal::is_vacancy`.
-/// \param occ_to_component_index_converter Lookup table for sublattice index
+/// \param _occ_to_component_index_converter Lookup table for sublattice index
 ///     and occupant index to component index. May be obtained from
 ///     `xtal::make_index_converter`.
+/// \param _vacancy_names Set of component names that should be recognized as
+///     vacancies. An error is throw if more than one component is a vacancy.
 CompositionCalculator::CompositionCalculator(
-    xtal::UnitCellCoordIndexConverter const &_unitcellcoord_index_converter,
     std::vector<std::string> const &_components,
-    std::vector<std::vector<Index>> const &_occ_to_component_index_converter)
-    : m_unitcellcoord_index_converter(_unitcellcoord_index_converter),
-      m_components(_components),
+    std::vector<std::vector<Index>> const &_occ_to_component_index_converter,
+    std::set<std::string> const &_vacancy_names)
+    : m_components(_components),
       m_occ_to_component_index_converter(_occ_to_component_index_converter),
-      m_volume(m_unitcellcoord_index_converter.total_sites() /
-               m_occ_to_component_index_converter.size()),
       m_vacancy_allowed(false) {
   for (int i = 0; i < m_components.size(); ++i) {
-    if (xtal::is_vacancy(m_components[i])) {
+    if (_vacancy_names.count(m_components[i])) {
+      if (m_vacancy_allowed) {
+        throw std::runtime_error(
+            "Error in CompositionCalculator: components contains multiple "
+            "vacancy species");
+      }
       m_vacancy_allowed = true;
       m_vacancy_index = i;
     }
@@ -41,13 +41,18 @@ std::vector<std::string> CompositionCalculator::components() const {
 ///     of components
 Eigen::VectorXd CompositionCalculator::mean_num_each_component(
     Eigen::VectorXi const &occupation) const {
-  return num_each_component(occupation).cast<double>() / m_volume;
+  Index n_sites = occupation.size();
+  Index n_sublat = m_occ_to_component_index_converter.size();
+  Index volume = n_sites / n_sublat;
+  return num_each_component(occupation).cast<double>() / volume;
 }
 
 /// \brief Returns the composition as total number, in the order of components
 Eigen::VectorXi CompositionCalculator::num_each_component(
     Eigen::VectorXi const &occupation) const {
-  Index n_sites = m_unitcellcoord_index_converter.total_sites();
+  Index n_sites = occupation.size();
+  Index n_sublat = m_occ_to_component_index_converter.size();
+  Index volume = n_sites / n_sublat;
   Index n_component = m_components.size();
 
   // initialize
@@ -55,7 +60,8 @@ Eigen::VectorXi CompositionCalculator::num_each_component(
 
   // count the number of each component
   for (Index i = 0; i < n_sites; i++) {
-    Index sublat = m_unitcellcoord_index_converter(i).sublattice();
+    // assumes occuaption vector organized in sublattice blocks
+    Index sublat = i / volume;
     Index component_index =
         m_occ_to_component_index_converter[sublat][occupation[i]];
     result[component_index] += 1;
