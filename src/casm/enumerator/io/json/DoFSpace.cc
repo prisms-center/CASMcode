e8904eb33f2bdd348b329aef70c8a129ff1a8a1d
bpuchala <bpuchala@users.noreply.github.com>
2022-05-23 09:19:52 -0400

    fix: handle default (null) DoFSpace basis input

--- src/casm/enumerator/io/json/DoFSpace.cc
+++ src/casm/enumerator/io/json/DoFSpace.cc
@@ -145,8 +145,10 @@ void parse(InputParser<DoFSpace> &parser,
   parser.optional(sites, "sites");
 
   Eigen::MatrixXd tmp;
-  parser.optional(tmp, "basis");
-  std::optional<Eigen::MatrixXd> basis = tmp.transpose();
+  std::optional<Eigen::MatrixXd> basis parser.optional(basis, "basis");
+  if (basis.has_value()) {
+    basis = basis.value().transpose();
+  }
 
   if (parser.valid()) {
     parser.value = notstd::make_unique<DoFSpace>(
